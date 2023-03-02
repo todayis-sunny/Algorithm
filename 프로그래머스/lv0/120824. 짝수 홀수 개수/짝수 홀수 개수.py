bcb0835b2150bd08d797a257d14324d40715d879
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2023-03-02 21:42:07 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,8 @@
+def solution(num_list):
+    answer = [0, 0]
+    for k in num_list:
+        if k % 2 == 0:
+            answer[0] += 1
+        else:
+            answer[1] += 1
+    return answer

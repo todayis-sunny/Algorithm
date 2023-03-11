ddc914271f8731ca9b685fea98d7cedb1e9a6655
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2023-03-11 15:45:24 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.03 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,5 @@
+def solution(numbers):
+    nums = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    for i, num in enumerate(nums):
+        numbers = numbers.replace(num, str(i))
+    return int(numbers)

6f1017972b2f31e55a5872ac2f5c2bef5985c93d
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2023-03-20 22:25:30 +0900

    [unrated] Title: 과일 장수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
@@ -0,0 +1,9 @@
+def solution(k, m, score):
+    
+    if m > len(score):
+        return 0
+    answer = 0
+    score = sorted(score, reverse = True)
+    for i in range(1, len(score)//m + 1):
+        answer += score[i*m-1] * m
+    return answer

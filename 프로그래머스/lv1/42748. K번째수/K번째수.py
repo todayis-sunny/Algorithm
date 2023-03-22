f822d0da79efcbc64500f40d7569fa6465947f61
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2023-03-22 15:46:37 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -0,0 +1,7 @@
+def solution(array, commands):
+    answer = []
+    for e in commands:
+        temp = array[e[0]-1:e[1]]
+        temp = sorted(temp)
+        answer.append(temp[e[2]-1])
+    return answer

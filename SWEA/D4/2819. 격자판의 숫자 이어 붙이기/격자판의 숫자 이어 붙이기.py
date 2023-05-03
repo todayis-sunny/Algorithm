16d3d13934b45ba627863c83def046c63efa8934
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2023-05-03 17:57:36 +0900

    [D4] Title: 격자판의 숫자 이어 붙이기, Time: 487 ms, Memory: 71,484 KB -BaekjoonHub

--- SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.py
+++ SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.py
@@ -2,26 +2,28 @@
 
 
 def dfs(x, y, s):
-    if x < 0 or x >= 4 or y < 0 or y >= 4:
+    if len(s) == 7:
+        result.add(s)
         return
-    s += arr[x][y]
-    if len(s) < 7:
-        dfs(x-1, y, s)
-        dfs(x+1, y, s)
-        dfs(x, y-1, s)
-        dfs(x, y+1, s)
-    else:
-        if s not in ans:
-            ans.append(s)
+    
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        
+        if 0 <= nx < 4 and 0 <= ny < 4:
+            dfs(nx, ny, s+arr[nx][ny])
 
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 tc = int(input())
 
 for t in range(1, tc + 1):
     arr = [list(map(str, input().split())) for _ in range(4)]
-    ans = []
+    result = set()
     for i in range(4):
         for j in range(4):
             dfs(i, j, '')
 
-    print(f'#{t} {len(ans)}')
+    print(f'#{t} {len(result)}')

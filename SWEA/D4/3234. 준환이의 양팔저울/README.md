b5427649bd8a85af71585377c526cf7c746a41a8
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2023-05-08 16:43:22 +0900

    [D4] Title: 준환이의 양팔저울, Time: 3,582 ms, Memory: 65,368 KB -BaekjoonHub

---
+++ SWEA/D4/3234. 준환이의 양팔저울/README.md
@@ -0,0 +1,11 @@
+# [D4] 준환이의 양팔저울 - 3234 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWAe7XSKfUUDFAUw) 
+
+### 성능 요약
+
+메모리: 65,368 KB, 시간: 3,582 ms, 코드길이: 976 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/3234. 준환이의 양팔저울/준환이의 양팔저울.py
@@ -0,0 +1,35 @@
+# 3234. 준환이의 양팔저울 <D4>
+
+# 좌측에 두는 행위 : left | 우측에 두는 행위 : right
+def dfs(left, right, cnt):
+    global case
+    # 마지막 순서 종료
+    if cnt == n:
+        case += 1
+        return
+    # 추를 좌우측에 상관없이 놓아도 괜찮은 경우 (남은 추 : N) -> 2**N * N!
+    if left >= limit - left:
+        case += (2 ** (n - cnt)) * factorial[n - cnt]
+        return
+
+    for idx in range(n):
+        if visited[idx]:
+            continue
+        # 앞선 행위가 가능하면 좌측에 두는 행위는 무조건 가능
+        visited[idx] = True
+        dfs(left + weight[idx], right, cnt + 1)
+        if left >= right + weight[idx]:
+            dfs(left, right + weight[idx], cnt + 1)
+        visited[idx] = False
+
+
+factorial = [0, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880]
+for t in range(1, int(input()) + 1):
+    n = int(input())
+    weight = list(map(int, input().split()))
+    limit = sum(weight)
+    case = 0
+    visited = [False] * n
+    dfs(0, 0, 0)
+
+    print(f'#{t} {case}')

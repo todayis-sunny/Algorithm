b188b5fc6744d6435e5415ce1c0f3df48b1b5598
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2023-05-14 21:22:55 +0900

    [D3] Title: 영준이와 신비한 뿔의 숲, Time: 147 ms, Memory: 58,524 KB -BaekjoonHub

--- SWEA/D3/3142. 영준이와 신비한 뿔의 숲/영준이와 신비한 뿔의 숲.py
+++ SWEA/D3/3142. 영준이와 신비한 뿔의 숲/영준이와 신비한 뿔의 숲.py
@@ -4,11 +4,6 @@ tc = int(input())
 
 for t in range(1, tc + 1):
     n, m = map(int, input().split())
-    ans1 = m
-    ans2 = 0
-    for i in range(m+1):
-        if n == ans1 + 2 * i:
-            break
-        ans1 -= 1
-        ans2 += 1
+    ans1 = 2*m - n
+    ans2 = m - ans1
     print(f'#{t} {ans1} {ans2}')

76798e324c8e2fa2146608291b016554b65a0709
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2023-05-26 18:09:55 +0900

    [level 2] Title: 카펫, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -0,0 +1,7 @@
+def solution(brown, yellow):
+    for y in range(1, yellow+1):
+        x = yellow // y
+        if yellow % y:
+            continue
+        if (2*x + 2*y + 4) == brown:
+            return [x+2, y+2]

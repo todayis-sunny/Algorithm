d69a562b068ac2e5873d328197350d0f7657be9a
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2023-06-03 15:31:23 +0900

    [unrated] Title: 귤 고르기, Time: 34.20 ms, Memory: 21.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -0,0 +1,17 @@
+def solution(k, tangerine):
+    cnt = k
+    dic = {}
+    ans = 0
+    for t in tangerine:
+        if t not in dic:
+            dic[t] = 1
+        else:
+            dic[t] += 1
+    dic = sorted(dic.items(), key = lambda x:x[1], reverse = True)
+    for i in range(len(dic)):
+        cnt -= dic[i][1]
+        if cnt <= 0:
+            ans = i+1
+            break
+            
+    return ans

d3cd455174072c4f67c40fbe000a65b9e263538d
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2023-06-11 15:38:25 +0900

    [level 2] Title: 택배상자, Time: 279.43 ms, Memory: 56.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+def solution(order):
+    answer = 0
+    dq = deque(order)
+    stack = []
+    limit = len(order)
+    max_box = 0
+    for box in order:
+        if max_box < box:
+            for b in range(max_box+1, box):
+                stack.append(b)
+            max_box = box
+            answer += 1
+        elif stack and stack[-1] == box:
+            stack.pop()
+            answer += 1
+        else:
+            break
+    return answer

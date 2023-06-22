c6cfe0c308a777e62e2c3acf662dfae515d19494
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2023-06-22 16:29:25 +0900

    [level 3] Title: 징검다리 건너기, Time: 85.57 ms, Memory: 18.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+def solution(stones, k):
+    answer = 1e9
+    dq = deque()
+    for idx, stone in enumerate(stones):
+        while dq and dq[-1][1] < stone:
+            dq.pop()
+        dq.append((idx,stone))
+            
+        if dq and dq[0][0] <= idx - k:
+            dq.popleft()
+        
+        if idx >= k-1:
+            answer = min(answer, dq[0][1])
+            
+    return answer

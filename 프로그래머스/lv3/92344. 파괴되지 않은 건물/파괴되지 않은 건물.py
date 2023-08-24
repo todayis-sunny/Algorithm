9528982e7f074a036725461ef2acf920a040a635
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2023-08-24 17:35:30 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 822.70 ms, Memory: 132 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
@@ -0,0 +1,30 @@
+def solution(board, skill):
+    count = 0
+    imos = [[0] * (len(board[0]) + 1) for _ in range(len(board) + 1)]
+    
+    for S in skill:
+        t, r1, c1, r2, c2, degree = S
+        imos[r1][c1] += ((-1)**t) * degree
+        imos[r1][c2 + 1] += ((-1)**(t+1)) * degree
+        imos[r2 + 1][c1] += ((-1)**(t+1)) * degree
+        imos[r2 + 1][c2 + 1] += ((-1)**t) * degree
+    
+    # 행 방향 누적합
+    for row in range(len(imos)):
+        for col in range(1, len(imos[0])):
+            imos[row][col] += imos[row][col - 1]
+    
+    # 열 방향 누적합
+    for col in range(len(imos[0])):
+        for row in range(1, len(imos)):
+            imos[row][col] += imos[row - 1][col]
+            
+    # 기존 보드와 더하기
+    for row in range(len(board)):
+        for col in range(len(board[0])):
+            board[row][col] += imos[row][col]
+            
+            if board[row][col] > 0:
+                count += 1
+    
+    return count

2a432bc391a9add7edea8d703e721971eb2f99f9
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2024-01-08 23:39:43 +0900

    [level 2] Title: 올바른 괄호, Time: 7.33 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,13 @@
+def solution(s):
+    stack = []
+    for c in s:
+        if c == "(":
+            stack.append(c)
+        else:
+            if stack:
+                stack.pop()
+            else:
+                return False
+    if stack:
+        return False
+    return True

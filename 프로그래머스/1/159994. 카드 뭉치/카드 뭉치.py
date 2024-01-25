47c8175fda68ab02b7ba0767216d4dfb45f38eb5
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2024-01-25 23:44:33 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+def solution(cards1, cards2, goal):
+    answer = 'Yes'
+    
+    cards1 = deque(cards1)
+    cards2 = deque(cards2)
+    for i in goal:
+        if (len(cards1) != 0) and (i == cards1[0]):
+            cards1.popleft()
+        elif (len(cards2) != 0) and (i == cards2[0]):
+            cards2.popleft()
+        else:
+            answer = 'No'
+            break
+        
+    return answer

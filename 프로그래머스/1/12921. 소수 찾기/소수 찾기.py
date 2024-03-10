d5f4b21d8e512fb4c3a0b351323206d88919b54f
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2024-03-10 19:23:44 +0900

    [level 1] Title: 소수 찾기, Time: 627.84 ms, Memory: 16.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 0
+    arr = [True] * (n+1)
+    
+    for num in range(2, n//2+1):
+        for i in range(num*2, n+1, num):
+            arr[i] = False
+    for j in range(2, n+1):
+        if arr[j]:
+            answer += 1
+    return answer

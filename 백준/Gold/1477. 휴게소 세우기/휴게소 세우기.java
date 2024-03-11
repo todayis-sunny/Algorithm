a757ec7880efcdc4f7bd5750f749dc5706d91bf4
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2024-03-11 16:19:43 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 128 ms, Memory: 14392 KB -BaekjoonHub

--- 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.java
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.java
@@ -1,60 +1,63 @@
-// 01477. [G4] 휴게소 세우기.
+//01477. [G4] 휴게소 세우기.
 
 import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringTokenizer st;
-    static int N, M, L;
-    static int[] space, init, trans, count;
+ static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ static StringTokenizer st;
+ static int N, M, L;
+ static int left, right, mid, cnt;
+ static int[] space, init, trans, count;
 
-    public static void main(String[] args) throws IOException {
-        st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int L = Integer.parseInt(st.nextToken());
-        space = new int[N + 2];
-        init = new int[N + 1];
-        trans = new int[N + 1];
-        count = new int[N + 1];
-        space[N + 1] = L;
+ public static void main(String[] args) throws IOException {
+     st = new StringTokenizer(br.readLine());
+     int N = Integer.parseInt(st.nextToken());
+     int M = Integer.parseInt(st.nextToken());
+     int L = Integer.parseInt(st.nextToken());
+     space = new int[N + 2];
+     init = new int[N + 1];
+     trans = new int[N + 1];
+     count = new int[N + 1];
+     space[N + 1] = L;
 
-        st = new StringTokenizer(br.readLine());
+     st = new StringTokenizer(br.readLine());
 
-        for (int n = 1; n <= N; n++) {
-            space[n] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(space);
-        int max = Integer.MIN_VALUE;
-        int idx = -1;
-        for (int i = 0; i <= N; i++) {
-            init[i] = trans[i] = space[i + 1] - space[i];
-            if (init[i] > max) {
-                max = init[i];
-                idx = i;
-            }
-        }
-        for (int m = 0; m < M; m++) {
-            trans[idx] = (int) Math.ceil(init[idx] / (double) (++count[idx] + 1));
-            max = 0;
-            for (int i = 0; i <= N; i++) {
-                if (trans[i] > max) {
-                    max = trans[i];
-                    idx = i;
-                }
-            }
-        }
-        int answer = 0;
-        for (int i = 0; i <= N; i++) {
-            answer = Math.max(answer, trans[i]);
-        }
-        bw.write(String.valueOf(answer));
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-    
+     for (int n = 1; n <= N; n++) {
+         space[n] = Integer.parseInt(st.nextToken());
+     }
+     Arrays.sort(space);
+     int max = Integer.MAX_VALUE;
+     for (int i = 0; i <= N; i++) {
+         init[i] = trans[i] = space[i + 1] - space[i];  
+     }
+     left = 1;
+     right = L;
+     
+     while (left <= right) {
+    	 cnt = 0;
+    	 mid = (left + right) / 2;
+    	 for (int n = 0; n < N+1; n++) {
+    		 int tmp = (int) Math.ceil(init[n] / (double) mid) - 1;
+    		 if (tmp < 0) {
+    			 tmp = 0;
+    		 }
+    		 cnt += tmp;
+    		 
+    		 
+    	 }
+    	 if (cnt > M) {
+			 left = mid + 1;
+			 continue;
+		 }
+    	 right = mid - 1;
+     }
+     bw.write(String.valueOf(left));
+     bw.flush();
+     bw.close();
+     br.close();
+ }
+ 
 }

6aa885c223cc503b9a73660ba18ab2e5921830c6
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2024-03-15 19:43:17 +0900

    [Gold III] Title: 크게 만들기, Time: 196 ms, Memory: 18276 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.java
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.java
@@ -7,40 +7,40 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-	 static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	 static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	 static StringTokenizer st;
-	 static StringBuilder sb;
-	 static int N, K, P, max, idx, number;
-	 static String answer, num;
-	 public static void main(String[] args) throws IOException {
-		answer = "";
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		P = N - K;
-		sb = new StringBuilder(P);
-		num = br.readLine();
-		idx = -1;
-		while (P > 0) {
-			max = -1;
-			for(int i = idx+1; i <= N-P; i++) {
-				number = Character.getNumericValue(num.charAt(i));
-				if (number > max) {
-					max = number;
-					idx = i;
-				}
-				if (number == 9) {
-					break;
-				}
-			}
-//			answer = answer.concat(String.valueOf(num.charAt(idx)));
-			sb.append(String.valueOf(num.charAt(idx)));
-			P--;
-		}
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-		br.close();
-	}
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb;
+    static int N, K, P, max, idx;
+    static String answer, num;
+    static char ch, tmp;
+    public static void main(String[] args) throws IOException {
+        answer = "";
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        P = N - K;
+        sb = new StringBuilder(P);
+        num = br.readLine();
+        idx = -1;
+        while (P > 0) {
+            tmp = '/'; // '0' 바로 직전.
+            for(int i = idx+1; i <= N-P; i++) {
+                ch = num.charAt(i);
+                if (ch > tmp) {
+                    tmp = ch;
+                    idx = i;
+                }
+                if (ch == '9') {
+                    break;
+                }
+            }
+            sb.append(tmp);
+            P--;
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }

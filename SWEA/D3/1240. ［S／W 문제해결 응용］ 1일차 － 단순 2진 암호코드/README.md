83826235ad7c2c9b848060fe9aadea56428f9373
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2024-03-18 11:06:24 +0900

    [D3] Title: [S/W 문제해결 응용] 1일차 - 단순 2진 암호코드, Time: 159 ms, Memory: 58,528 KB -BaekjoonHub

--- SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/README.md
+++ SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/README.md
@@ -6,6 +6,10 @@
 
 메모리: 58,528 KB, 시간: 159 ms, 코드길이: 822 Bytes
 
+### 제출 일자
+
+2023-04-26 15:10
+
 
 
 > 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
--- SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.py
+++ SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.py
@@ -1,31 +1,30 @@
 # 1240. 단순 2진 암호코드 <D3>
 
 tc = int(input())
-dic = {'0001101' : 0, '0011001' : 1,
-       '0010011' : 2, '0111101' : 3,
-       '0100011' : 4, '0110001' : 5,
-       '0101111' : 6, '0111011' : 7,
-       '0110111' : 8, '0001011' : 9}
+dic = {'0001101': 0, '0011001': 1,
+       '0010011': 2, '0111101': 3,
+       '0100011': 4, '0110001': 5,
+       '0101111': 6, '0111011': 7,
+       '0110111': 8, '0001011': 9}
 for t in range(1, tc + 1):
     n, m = map(int, input().split())
     arr = [str(input()) for _ in range(n)]
     for i in range(n):
         if not arr[i].count('1'):
             continue
-        for j in range(1, m+1):
+        for j in range(1, m + 1):
             if arr[i][-j] == '1':
                 b = m - j
                 a = b - 56
                 break
-        tmp = arr[i][a+1:b+1]
+        tmp = arr[i][a + 1:b + 1]
         break
     pwd = []
     for k in range(0, 56, 7):
-        pwd.append(dic[tmp[k:k+7]])
+        pwd.append(dic[tmp[k:k + 7]])
     ans = sum(pwd[0::2]) * 3 + sum(pwd[1::2])
 
     if not ans % 10:
         print(f'#{t} {sum(pwd)}')
     else:
         print(f'#{t} 0')
-

e5a5a199d06488d5ef024da5ca98d2dee54fcab3
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2024-03-19 09:17:28 +0900

    [Silver I] Title: RGB거리, Time: 92 ms, Memory: 12124 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -11,28 +11,22 @@ public class Main {
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer st;
 	
-	static int N, R, G, B, r, g, b, answer;
-	static int[] dp;
+	static int N, idx1, idx2, last;
+	static int[][] dp;
 	public static void main(String[] args) throws IOException {
 		N = Integer.parseInt(br.readLine());
-		dp = new int[3];
-		answer = Integer.MAX_VALUE;
+		dp = new int[2][3];
 		for(int n = 1; n <= N; n++) {
+			idx1 = n % 2;
+			idx2 = (n+1) % 2;
 			st = new StringTokenizer(br.readLine());
-			R = Integer.parseInt(st.nextToken());
-			G = Integer.parseInt(st.nextToken());
-			B = Integer.parseInt(st.nextToken());
+			dp[idx1][0] = Integer.parseInt(st.nextToken()) + Math.min(dp[idx2][1], dp[idx2][2]);
+			dp[idx1][1] = Integer.parseInt(st.nextToken()) + Math.min(dp[idx2][0], dp[idx2][2]);
+			dp[idx1][2] = Integer.parseInt(st.nextToken()) + Math.min(dp[idx2][0], dp[idx2][1]);
 			
-			r = Math.min(dp[1], dp[2]) + R;
-			g = Math.min(dp[0], dp[2]) + G;
-			b = Math.min(dp[0], dp[1]) + B;
-			
-			dp[0] = r;
-			dp[1] = g;
-			dp[2] = b;
 		}
-		answer = Math.min(dp[0], Math.min(dp[1], dp[2]));
-		bw.write(String.valueOf(answer));
+		last = N % 2;
+		bw.write(String.valueOf(Math.min(dp[last][0], Math.min(dp[last][1], dp[last][2]))));
 		bw.flush();
 		bw.close();
 		br.close();

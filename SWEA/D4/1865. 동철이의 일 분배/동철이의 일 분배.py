536010ca196fc569885d5810aba8aa81618893c1
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2024-03-20 10:33:22 +0900

    [D4] Title: 동철이의 일 분배, Time: 4,369 ms, Memory: 75,076 KB -BaekjoonHub

--- SWEA/D4/1865. 동철이의 일 분배/동철이의 일 분배.py
+++ SWEA/D4/1865. 동철이의 일 분배/동철이의 일 분배.py
@@ -1,24 +1,21 @@
 # 1865. [diff_4] 동철이의 일 분배.
 
-def dfs(idx, value):
-    global answer
-    if value <= answer:
-        return
-    if idx == N:
-        answer = value
-    for i in range(N):
-        if not visited[i]:
-            visited[i] = True
-            dfs(idx+1, value*arr[idx][i])
-            visited[i] = False
+def dfs(n, bits):
+    if n == N:
+        return 1
+    if mem[bits] == 0:
+        mx = 0
+        for i in range(N):
+            if (bits & (1 << i)) == 0:
+                mx = max(mx, dfs(n+1, bits + (1 << i)) * arr[n][i])
+        mem[bits] = mx
+    return mem[bits]
 
 
 for tc in range(1, int(input())+1):
     N = int(input())
-    answer = 0
-    visited = [False] * N
-
     arr = [[num / 100 for num in map(int, input().split())] for _ in range(N)]
-    dfs(0, 1)
-    answer *= 100
-    print(f"#{tc} {round(answer, 6):.6f}")
+    mem = [0]*(2**N)
+
+    answer = dfs(0, 0) * 100
+    print(f"#{tc} {answer:.6f}")

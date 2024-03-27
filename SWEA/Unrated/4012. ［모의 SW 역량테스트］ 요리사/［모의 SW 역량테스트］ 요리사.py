925562bd04e9b150037b5f031d86f39a2f043215
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2024-03-27 13:03:31 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 292 ms, Memory: 75,692 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.py
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.py
@@ -3,8 +3,9 @@
 def bitMaker(n, bits):
     global answer
     if n == halfN:
-        if bitmask[((1 << N)-1) ^ bits] != 0:
-            value = abs(bitmask[bits] - bitmask[((1 << N) - 1) ^ bits])
+        revBits = ((1 << N)-1) ^ bits
+        if bitmask[revBits] != 0:
+            value = abs(bitmask[bits] - bitmask[revBits])
             answer = min(answer, value)
         return
     for i in range(N):
@@ -15,7 +16,7 @@ def bitMaker(n, bits):
             value = 0
             for j in range(N):
                 if bits & (1 << j) != 0:  # 선택되지 않은 식재료는 무시.
-                    value += synergy[i][j] + synergy[j][i]
+                    value += pair[i][j]
             bitmask[newBits] = bitmask[bits] + value
             bitMaker(n+1, newBits)
 
@@ -24,6 +25,10 @@ for tc in range(1, int(input()) + 1):
     N = int(input())
     halfN = N//2
     synergy = [list(map(int, input().split())) for _ in range(N)]
+    pair = [[0] * N for _ in range(N)]
+    for i in range(N):
+        for j in range(i + 1, N):
+            pair[i][j] = pair[j][i] = synergy[i][j] + synergy[j][i]
     bitmask = [0] * (2**N - (2**(N//2) - 1))    # (2**N - 1 - (2**(N//2) - 1) + 1)
     answer = 1e9
     bitMaker(0, 0)

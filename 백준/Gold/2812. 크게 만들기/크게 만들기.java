876e879755ed8e1127362404f0c8f29b8da1fbab
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2024-03-29 09:03:15 +0900

    [Gold III] Title: 크게 만들기, Time: 176 ms, Memory: 18688 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.java
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.java
@@ -1,46 +1,40 @@
-// 02812. [G3] 크게 만들기.
+//02812. [G3] 크게 만들기.
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringTokenizer st;
-    static StringBuilder sb;
-    static int N, K, P, max, idx;
-    static String answer, num;
-    static char ch, tmp;
-    public static void main(String[] args) throws IOException {
-        answer = "";
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        P = N - K;
-        sb = new StringBuilder(P);
-        num = br.readLine();
-        idx = -1;
-        while (P > 0) {
-            tmp = '/'; // '0' 바로 직전.
-            for(int i = idx+1; i <= N-P; i++) {
-                ch = num.charAt(i);
-                if (ch > tmp) {
-                    tmp = ch;
-                    idx = i;
-                }
-                if (ch == '9') {
-                    break;
-                }
-            }
-            sb.append(tmp);
-            P--;
-        }
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb;
+	static int N, K, P, idx;
+	static String num;
+	static char ch, tmp;
+
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		P = N - K;
+		sb = new StringBuilder(P);
+		num = br.readLine();
+		idx = -1;
+		while (P > 0) {
+			tmp = '/'; // '0' 바로 직전.
+			for (int i = idx + 1; i <= N - P; i++) {
+				ch = num.charAt(i);
+				if (ch > tmp) {
+					tmp = ch;
+					idx = i;
+					if (ch == '9') {
+						break;
+					}
+				}
+			}
+			sb.append(tmp);
+			P--;
+		}
+		System.out.println(sb.toString());
+	}
 }

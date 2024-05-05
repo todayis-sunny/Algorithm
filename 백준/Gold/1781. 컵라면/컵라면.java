3bd07996408976b152f45338ee9193fcacd7db62
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2024-05-05 21:52:22 +0900

    [Gold II] Title: 컵라면, Time: 1032 ms, Memory: 152896 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.java
+++ 백준/Gold/1781. 컵라면/컵라면.java
@@ -13,7 +13,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
         problem = new ArrayList<>();
-        pq = new PriorityQueue<>(Comparator.reverseOrder());
+        pq = new PriorityQueue<>(Comparator.reverseOrder());  // 컵라면이 많은 것부터 처리해야 하므로 최대 우선순위 큐.
         answer = 0;
         for (int n = 0; n < N; n++) {
             st = new StringTokenizer(br.readLine());
@@ -21,17 +21,17 @@ public class Main {
             cupNoodle = Integer.parseInt(st.nextToken());
             problem.add(new Item(deadLine, cupNoodle));
         }
-        problem.add(new Item(0, 0));
-        problem.sort(Comparator.comparing(Item::getIdx).reversed());
-        
-        int right = 200_001;
-        int left = -1;
-        int cnt = 0;
+        problem.add(new Item(0, 0)); // 마지막 컵라면을 비교해주기 위한 의미없는 값 넣어주기.
+        problem.sort(Comparator.comparing(Item::getIdx).reversed()); // index가 큰것 부터 접근하기 -> 시간순으로 볼것이 아니라, 현재에서 과거의 방향으로.
 
+        int prev = 200_001;
+        int next = -1;
+        int cnt = 0;
+        // prev는 기존 컵라면의 데드라인, next는 선택할 컵라면의 데드라인.
         for (int n = 0; n <= N; n++) {
-            left = problem.get(n).idx;
-            if (left != right) {
-                cnt = right - left;
+            next = problem.get(n).idx;
+            if (next != prev) {
+                cnt = prev - next;
                 for (int i = 0; i < cnt; i++) {
                     if (pq.isEmpty()) {
                         break;
@@ -40,7 +40,7 @@ public class Main {
                     }
                 }
             }
-            right = left;
+            prev = next;
             int cup = problem.get(n).cnt;
             pq.offer(cup);
         }

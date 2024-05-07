fd7308a1ba76f8d62e1e493f5bc6e98f65219cd7
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2024-05-07 21:47:02 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = []
+    while n != 1:
+        answer.append(n)
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n = 3*n + 1
+    answer.append(n)
+    return answer

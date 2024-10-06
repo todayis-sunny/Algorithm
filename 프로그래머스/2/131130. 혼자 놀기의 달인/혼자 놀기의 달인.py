a1b574cb6cb9e04a128a0e82414c562b2821805e
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2024-10-06 22:14:50 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.py
@@ -0,0 +1,26 @@
+def solution(cards):
+    n = len(cards)
+    v = [0 for _ in range(n+1)]
+    
+    stack = []
+    
+    def dfs(s, cnt):
+        nextC = cards[s-1]
+        if v[nextC]:
+            stack.append(cnt)
+            return
+        v[nextC] = 1
+        dfs(nextC, cnt+1)
+    
+    for card in cards:
+        if v[card]:
+            continue
+        v[card] = 1
+        dfs(card, 1)
+    
+    answer = 0
+    if len(stack) >= 2:
+        stack.sort()
+        answer = stack[-1] * stack[-2]
+        
+    return answer

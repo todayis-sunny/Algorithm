8e5a4a8fbb41c8f3a6f4b8468fd7674b11a276ab
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2024-10-12 17:48:20 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 328 ms, Memory: 64460 KB -BaekjoonHub

--- 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.java
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.java
@@ -1,6 +1,7 @@
 // 03015. [P5] 오아시스 재결합
 import java.io.*;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -8,30 +9,30 @@ public class Main {
     static int N;
     static long ans;
     static int[] human;
-    static Stack<Human> stack;
+    static Deque<Human> stack;
 
     public static void main(String[] args) throws IOException {
         // 0. 초기화 단계
         N = Integer.parseInt(br.readLine());
         human = new int[N];
-        stack = new Stack<>();
+        stack = new ArrayDeque<>();
         ans = 0;
         // 1. 입력 단계
         for (int n = 0; n < N; n++) {
             int data = Integer.parseInt(br.readLine());
 
-            while (!stack.isEmpty() && data > stack.peek().height) {
-                ans += stack.pop().count;
+            while (!stack.isEmpty() && data > stack.peekLast().height) {
+                ans += stack.pollLast().count;
             }
 
             if (stack.isEmpty()) { // 스택이 비어 있으면, 데이터 추가.
-                stack.push(new Human(data));
+                stack.offerLast(new Human(data));
             } else {
-                if (data < stack.peek().height) { // 더 큰 사람을 만나면 스택에 추가하고, ans 증가
-                    stack.push(new Human(data));
+                if (data < stack.peekLast().height) { // 더 큰 사람을 만나면 스택에 추가하고, ans 증가
+                    stack.offerLast(new Human(data));
                     ans++;
                 } else { // 키가 같은 사람을 만나면 count 증가
-                    ans += stack.peek().count++;
+                    ans += stack.peekLast().count++;
                     if (stack.size() > 1) {
                         ans ++;
                     }

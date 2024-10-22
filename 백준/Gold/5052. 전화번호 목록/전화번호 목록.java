80c63694732584c2f2cd631279eda803f805569d
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2024-10-22 23:19:11 +0900

    [Gold IV] Title: 전화번호 목록, Time: 380 ms, Memory: 225128 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -1,4 +1,5 @@
 // 05052. [G4] 전화번호 목록.
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -72,79 +73,18 @@ public class Main {
                     return false;
                 }
                 // 문자열의 각 단어를 가져와서 자식노드 중에 있는지 체크
-                if(node.child.putIfAbsent(ch, new Node()) != null && i == str.length()-1) {
-                    return false;
-                }
+                node.child.putIfAbsent(ch, new Node());
+
                 // 있을 때 : node = node.child.get(str.charAt(i)); putIfAbsent는 값 존재시에 value를 반환
                 // 없을 때 : 새로운 노드를 생성해서 넣음
                 node = node.child.get(ch);
             }
-            node.endOfWord = true;
-            return true;
-        }
-
-        /**
-         * 탐색 메서드
-         */
-        public boolean search(String str) {
-            Node node = this.root;
-
-            for (int i = 0; i < str.length(); i++) {
-                char ch = str.charAt(i);
-                // 자식노드에 ch가 있을 때 계속 탐색
-                if (node.child.containsKey(ch)) {
-                    node = node.child.get(ch);
-                } else {
-                    return false;
-                }
-            }
-            // 마지막 노드까지 도달 시, 마지막 노드의 endOfWord를 반환
-            return node.endOfWord;
-        }
-
-        /**
-         * 삭제 메서드
-         */
-        public boolean delete(String str) {
-            return delete(this.root, str, 0);
-        }
-
-        /**
-         * 내부적으로 재귀를 통해 삭제하는 메서드
-         */
-        private boolean delete(Node node, String str, int idx) {
-            char ch = str.charAt(idx);
-
-            // 현재 노드의 자식노드에서 ch를 지워야하는데 없으면 false 반환
-            if (!node.child.containsKey(ch)) {
+            // 마지막 문자열의 문자를 넣을 때, 일관성이 없는 목록인지 체크.
+            if (!node.child.isEmpty()) {
+                // 현재 단어가 다른 단어의 접두어인 경우
                 return false;
             }
-
-            Node curr = node.child.get(ch);
-            idx++;
-            // 문자열 끝에 도달했을 때
-            if (idx == str.length()) {
-                if (!curr.endOfWord) {
-                    return false;
-                }
-                // endOfWord를 flase로 바꿔주면 지우려는 문자열을 찾을 수 없게 됨
-                curr.endOfWord = false;
-
-                // 지우려는 문자열의 마지막에서 더 뻗어나가는 경우
-                if (curr.child.isEmpty()) {
-                    node.child.remove(ch);
-                }
-            } else { // 문자열 끝에 도달하지 않았을 때
-                // 재귀적으로 현재 노드부터 다시 호출
-                if (!this.delete(curr, str, idx)) { // 삭제에 실패했을 경우
-                    return false;
-                }
-                // 삭제에 성공했고, 자식노드가 비어 있으면 현재 노드를 삭제
-                // node는 curr의 부모노드임. 즉, curr노드를 node의 자식 Map에서 삭제하는 것
-                if (!curr.endOfWord && curr.child.isEmpty()) {
-                    node.child.remove(ch);
-                }
-            }
+            node.endOfWord = true;
             return true;
         }
 

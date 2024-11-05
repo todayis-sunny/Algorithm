419f79dca1848a810b453067e38629c9fba84a66
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2024-11-05 23:11:50 +0900

    [Gold II] Title: 저울, Time: 68 ms, Memory: 11916 KB -BaekjoonHub

--- 백준/Gold/2437. 저울/저울.java
+++ 백준/Gold/2437. 저울/저울.java
@@ -8,18 +8,9 @@ public class Main {
     static StringTokenizer st;
     static int N;
     static int[] data;
-    /*
-    구하고자 하는건 최솟값을 구해야한다. 적당하게 큰 값은 배열에 저장할 필요가 있을까?
-    트리맵을 사용?
-     */
-
+    // 수학적 귀납법으로 접근.
     public static void main(String[] args) throws IOException {
-        N = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        data = new int[N];
-        for (int n = 0; n < N; n++) {
-            data[n] = Integer.parseInt(st.nextToken());
-        }
+        input();
         Arrays.sort(data);
         int target = 1;
         for (int n = 0; n < N; n++) {
@@ -32,4 +23,12 @@ public class Main {
         bw.flush();
     }
 
+    static void input() throws IOException {
+        N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        data = new int[N];
+        for (int n = 0; n < N; n++) {
+            data[n] = Integer.parseInt(st.nextToken());
+        }
+    }
 }

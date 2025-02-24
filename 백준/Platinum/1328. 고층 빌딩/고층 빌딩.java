9c5b9b52110b384e639416b79394647ac9cc7bf9
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-02-24 20:27:18 +0900

    [Platinum V] Title: 고층 빌딩, Time: 100 ms, Memory: 20540 KB -BaekjoonHub

---
+++ 백준/Platinum/1328. 고층 빌딩/고층 빌딩.java
@@ -0,0 +1,54 @@
+// 01328. [P5] 고층 빌딩.
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int N, L, R; // N: 빌딩의 개수, L: 가장 좌측에서 봤을 때 보이는 빌딩의 수, R: 가장 우측에서 봤을 때 보이는 빌딩의 수
+    static final int MOD = 1_000_000_007;
+    static long[][][] dp; // [N][L][R]
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solve();
+        output();
+    }
+
+    static void input() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        dp = new long[N + 1][N + 1][N + 1];
+    }
+
+    static void solve() {
+        dp[1][1][1] = 1;
+        for (int n = 1; n < N; n++) {
+            for (int l = 1; l <= n; l++) {
+                for (int r = 1; r <= n; r++) {
+                    if (dp[n][l][r] == 0) { // 경우의 수가 없으면 무시
+                        continue;
+                    }
+                    // 가장 좌측에 높이 1인 빌딩 추가
+                    dp[n + 1][l + 1][r] += dp[n][l][r];
+                    dp[n + 1][l + 1][r] %= MOD;
+                    // 가장 우측에 높이 1인 빌딩 추가
+                    dp[n + 1][l][r + 1] += dp[n][l][r];
+                    dp[n + 1][l][r + 1] %= MOD;
+                    // 그 외에 높이 1인 빌딩 추가 (빌딩이 n+1개 일때, 높이 1인 빌딩이 보이지 않게 하려면 n-1개의 위치)
+                    dp[n + 1][l][r] += (dp[n][l][r] * (n - 1));
+                    dp[n + 1][l][r] %= MOD;
+                }
+            }
+        }
+    }
+
+    static void output() throws IOException {
+        bw.write(String.valueOf(dp[N][L][R]));
+        bw.flush();
+    }
+}

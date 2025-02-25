63f3444ceeeaa2ec29c6a060b7cd10234239f0d0
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-02-25 19:37:04 +0900

    [level 2] Title: 완전범죄, Time: 2.15 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.py
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.py
@@ -1,5 +1,6 @@
 def solution(info, n, m):
-    dp = [[-1] * n for _ in range(len(info))] # [i][n] : i번째 물건을 훔칠때, A가 남긴 흔적의 갯수 -> B가 남긴 흔적의 갯수
+    INF = 1e9
+    dp = [[INF] * n for _ in range(len(info))] # [i][n] : i번째 물건을 훔칠때, A가 남긴 흔적의 갯수 -> B가 남긴 흔적의 갯수
     # 초기화
     if info[0][0] < n:
         dp[0][info[0][0]] = 0 # A가 물건을 훔침
@@ -12,23 +13,18 @@ def solution(info, n, m):
         nB = info[i][1] # B가 남길 흔적
         for k in range(n-1, -1, -1):
             # 불가능한 경우 무시
-            if dp[i - 1][k] == -1: 
+            if dp[i - 1][k] == INF: 
                 continue
             # A가 한도초과가 아닐 경우
             if k + nA < n:
-                if dp[i][k + nA] != -1:
-                    dp[i][k + nA] = min(dp[i][k + nA], dp[i - 1][k])
-                else:
-                    dp[i][k + nA] = dp[i - 1][k]
+                dp[i][k + nA] = min(dp[i][k + nA], dp[i - 1][k])
             # B가 한도 초과가 아닐 경우
             if dp[i - 1][k] + nB < m:
-                if dp[i][k] != -1:
-                    dp[i][k] = min(dp[i][k], dp[i - 1][k] + nB)
-                else:
-                    dp[i][k] = dp[i - 1][k] + nB
+                dp[i][k] = min(dp[i][k], dp[i - 1][k] + nB)
+
     # 정답 반환                 
     for ans in range(n):
-        if dp[-1][ans] != -1:
+        if dp[-1][ans] != INF:
             return ans
     return -1
 

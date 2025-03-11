db4be017e0082ba0ddec74eb2357203387a5c591
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-03-12 00:05:55 +0900

    [level 2] Title: 요격 시스템, Time: 347.40 ms, Memory: 91.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -0,0 +1,10 @@
+def solution(targets):
+    answer = 0
+    targets.sort(key=lambda x: x[1])
+    e = 0
+    
+    for target in targets:
+        if target[0] >= e:
+            answer += 1
+            e = target[1]
+    return answer

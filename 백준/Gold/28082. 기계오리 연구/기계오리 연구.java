6a8b55af46313c72032a5de91b713c733d466147
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-03-25 22:18:46 +0900

    [Gold I] Title: 기계오리 연구, Time: 248 ms, Memory: 20488 KB -BaekjoonHub

--- 백준/Gold/28082. 기계오리 연구/기계오리 연구.java
+++ 백준/Gold/28082. 기계오리 연구/기계오리 연구.java
@@ -1,4 +1,5 @@
 // 28082. [G1] 기계오리 연구.
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -7,56 +8,49 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static StringTokenizer st;
-	static StringBuilder sb = new StringBuilder();
-	static int N, K, answer;
-	static final int MAX_POWER = 500*100;
-	static int[] battery, power;
-	public static void main(String[] args) throws IOException {
-		answer = 0;
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		
-		st = new StringTokenizer(br.readLine());
-		battery = new int[N];
-		power = new int[MAX_POWER + 1];
-		for (int n = 0; n < N; n++) {
-			battery[n] = Integer.parseInt(st.nextToken());
-		}
-		
-		for (int n = 0; n < N; n++) {
-			int curr = battery[n];
-			for (int p = MAX_POWER; p > 0; p--) {
-				if(power[p] == 0) {
-					continue;
-				}
-				if (p + curr > MAX_POWER) {
-					continue;
-				}
-				if(power[p] == K) {
-					continue;
-				}
-				if(power[p+curr] != 0 && power[p+curr] <= power[p] + 1) {
-					continue;
-				}
-				power[p+curr] = power[p] + 1;
-			}
-			power[curr] = 1;
-		}
-		
-		for (int p = 0; p <= MAX_POWER; p++) {
-			if(power[p] > 0) {
-				sb.append(p + " ");
-				answer++;
-			}
-		}
-		bw.write(answer + "\n");
-		bw.write(sb.toString());
-		bw.flush();
-		br.close();
-		bw.close();
-	}
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static int N, K, answer;
+    static final int MAX_POWER = 500 * 100;
+    static int[] battery, power;
+
+    public static void main(String[] args) throws IOException {
+        answer = 0;
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        battery = new int[N];
+        power = new int[MAX_POWER + 1];
+
+        for (int n = 0; n < N; n++) {
+            int curr = Integer.parseInt(st.nextToken());
+            for (int p = MAX_POWER - curr; p > 0; p--) {
+                if (power[p] == 0) {
+                    continue;
+                }
+                if (power[p] == K) {
+                    continue;
+                }
+                if (power[p + curr] != 0 && power[p + curr] <= power[p] + 1) {
+                    continue;
+                }
+                power[p + curr] = power[p] + 1;
+            }
+            power[curr] = 1;
+        }
+
+        for (int p = 0; p <= MAX_POWER; p++) {
+            if (power[p] > 0) {
+                sb.append(p + " ");
+                answer++;
+            }
+        }
+        bw.write(answer + "\n");
+        bw.write(sb.toString());
+        bw.flush();
+    }
 }

5bb2c293ba71c98a4431970aecffcb05061be7c8
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-03-25 22:23:48 +0900

    [Gold I] Title: 기계오리 연구, Time: 244 ms, Memory: 19916 KB -BaekjoonHub

--- 백준/Gold/28082. 기계오리 연구/기계오리 연구.java
+++ 백준/Gold/28082. 기계오리 연구/기계오리 연구.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,7 +15,8 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
     static int N, K, answer;
     static final int MAX_POWER = 500 * 100;
-    static int[] battery, power;
+    static final int INF = Integer.MAX_VALUE;
+    static int[] power;
 
     public static void main(String[] args) throws IOException {
         answer = 0;
@@ -23,19 +25,18 @@ public class Main {
         K = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        battery = new int[N];
         power = new int[MAX_POWER + 1];
-
+        Arrays.fill(power, INF);
         for (int n = 0; n < N; n++) {
             int curr = Integer.parseInt(st.nextToken());
             for (int p = MAX_POWER - curr; p > 0; p--) {
-                if (power[p] == 0) {
+                if (power[p] == INF) {
                     continue;
                 }
                 if (power[p] == K) {
                     continue;
                 }
-                if (power[p + curr] != 0 && power[p + curr] <= power[p] + 1) {
+                if (power[p + curr] <= power[p] + 1) {
                     continue;
                 }
                 power[p + curr] = power[p] + 1;
@@ -44,7 +45,7 @@ public class Main {
         }
 
         for (int p = 0; p <= MAX_POWER; p++) {
-            if (power[p] > 0) {
+            if (power[p] != INF) {
                 sb.append(p + " ");
                 answer++;
             }

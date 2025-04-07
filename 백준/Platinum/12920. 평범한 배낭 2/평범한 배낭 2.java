4b88dbe5294dceba32472004eb982f03c63f818e
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-04-07 13:34:16 +0900

    [Platinum IV] Title: 평범한 배낭 2, Time: 128 ms, Memory: 13124 KB -BaekjoonHub

--- 백준/Platinum/12920. 평범한 배낭 2/평범한 배낭 2.java
+++ 백준/Platinum/12920. 평범한 배낭 2/평범한 배낭 2.java
@@ -1,4 +1,5 @@
 // 12920. [P4] 평범한 배낭2.
+
 import java.io.*;
 import java.util.StringTokenizer;
 
@@ -6,35 +7,43 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
+    // N: 민호의 집에 있는 물건의 종류의 수, M: 민호가 들 수 있는 가방의 최대 무게.
+    static int N, M;
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
-        st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[] dp = new int[M+1];
-        for (int n = 1; n <= N; n++) {
-            st = new StringTokenizer(br.readLine());
-            int weight = Integer.parseInt(st.nextToken());
-            int value = Integer.parseInt(st.nextToken());
-            int count = Integer.parseInt(st.nextToken());
-            int cnt = 1;
-            while (count > 0) {
-                cnt = Math.min(cnt, count); // 1, 2, 4... 2의 배수로 빼면서 남은갯수를 더이상 쪼갤 수 없으면 마무리.
-                int wc = weight * cnt;
-                int vc = value * cnt;
-                for (int m = M; m >= wc; m--) {
-                    if (m >= wc) { // Top-down 방식. -> 중복된 사용을 방지.
-                        dp[m] = Math.max(dp[m], dp[m - wc] + vc);
-                    }
-                }
-                count -= cnt;
-                cnt *= 2;
-            }
-        }
-        bw.write(String.valueOf(dp[M]));
-        bw.flush();
-        bw.close();
-        br.close();
+        input();
+        output();
     }
 
+    static void input() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        dp = new int[M + 1];
+        for (int n = 0; n < N; n++) {
+            st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken()); // 무게 입력
+            int v = Integer.parseInt(st.nextToken()); // 가치 입력
+            int c = Integer.parseInt(st.nextToken()); // 개수 입력
+            int cnt = 1;
+            while (c > 0) {
+                // 1, 2, 4, 8, 16.. 2의 거듭제곱으로 빼면서 남은 개수를 더 이상 쪼갤 수 없으면 마무리.
+                cnt = Math.min(cnt, c);
+                int wc = w * cnt; // 무게와 개수의 곱 -> 하나의 물건의 무게로 인지
+                int vc = v * cnt; // 가치와 개수의 곱 -> 하나의 물건의 가치로 인지
+                // top-down 방식 -> 중복된 사용 금지
+                for (int m = M; m >= wc; m--) {
+                    dp[m] = Math.max(dp[m], dp[m - wc] + vc);
+                }
+                c -= cnt;
+                cnt <<= 1;
+            }
+        }
+    }
+
+    static void output() throws IOException {
+        bw.write(String.valueOf(dp[M]));
+        bw.flush();
+    }
 }

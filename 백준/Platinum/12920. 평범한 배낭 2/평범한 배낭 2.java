245cab8081a697266d16d019502f2e8e0497a575
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-04-07 14:01:14 +0900

    [Platinum IV] Title: 평범한 배낭 2, Time: 96 ms, Memory: 11956 KB -BaekjoonHub

--- 백준/Platinum/12920. 평범한 배낭 2/평범한 배낭 2.java
+++ 백준/Platinum/12920. 평범한 배낭 2/평범한 배낭 2.java
@@ -1,18 +1,22 @@
 // 12920. [P4] 평범한 배낭2.
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
-    // N: 민호의 집에 있는 물건의 종류의 수, M: 민호가 들 수 있는 가방의 최대 무게.
+
     static int N, M;
     static int[] dp;
+    static List<Item> items = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         input();
+        solve();
         output();
     }
 
@@ -21,23 +25,27 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         dp = new int[M + 1];
-        for (int n = 0; n < N; n++) {
+
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int w = Integer.parseInt(st.nextToken()); // 무게 입력
-            int v = Integer.parseInt(st.nextToken()); // 가치 입력
-            int c = Integer.parseInt(st.nextToken()); // 개수 입력
+            int w = Integer.parseInt(st.nextToken()); // 물건의 무게
+            int v = Integer.parseInt(st.nextToken()); // 물건의 가치
+            int c = Integer.parseInt(st.nextToken()); // 물건의 개수
             int cnt = 1;
             while (c > 0) {
                 // 1, 2, 4, 8, 16.. 2의 거듭제곱으로 빼면서 남은 개수를 더 이상 쪼갤 수 없으면 마무리.
-                cnt = Math.min(cnt, c);
-                int wc = w * cnt; // 무게와 개수의 곱 -> 하나의 물건의 무게로 인지
-                int vc = v * cnt; // 가치와 개수의 곱 -> 하나의 물건의 가치로 인지
-                // top-down 방식 -> 중복된 사용 금지
-                for (int m = M; m >= wc; m--) {
-                    dp[m] = Math.max(dp[m], dp[m - wc] + vc);
-                }
-                c -= cnt;
+                int use = Math.min(cnt, c); // 사용할 개수
+                items.add(new Item(w * use, v * use));
+                c -= use;
                 cnt <<= 1;
+            }
+        }
+    }
+
+    static void solve() {
+        for (Item it : items) {
+            for (int m = M; m >= it.weight; m--) {
+                dp[m] = Math.max(dp[m], dp[m - it.weight] + it.value);
             }
         }
     }
@@ -45,5 +53,16 @@ public class Main {
     static void output() throws IOException {
         bw.write(String.valueOf(dp[M]));
         bw.flush();
+        bw.close();
+    }
+
+    static class Item {
+        int weight;
+        int value;
+
+        public Item(int weight, int value) {
+            this.weight = weight;
+            this.value = value;
+        }
     }
 }

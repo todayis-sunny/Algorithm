6aecd69e85e9f5334155bd66302d5e3a7e616394
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-04-14 23:40:48 +0900

    [Gold III] Title: 크게 만들기, Time: 152 ms, Memory: 17672 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.java
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.java
@@ -1,40 +1,53 @@
-//02812. [G3] 크게 만들기.
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+// 02812. [G3] 크게 만들기.
+
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static StringBuilder sb;
-	static int N, K, P, idx;
-	static String num;
-	static char ch, tmp;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int N, K, P, idx; // N: 숫자 자리수, K: 소거할 숫자 개수, P: 남길 숫자 개수(N - K), idx: 마지막에 사용된 index
+    static String inputNum; // 입력할 숫자 String
+    static char ch, tmp; // ch: 문자(사실은 숫자), tmp: 비교할 임시값
+    static StringBuilder sb; // 정답
+    public static void main(String[] args) throws IOException {
+        input();
+        solve();
+        output();
+    }
 
-	public static void main(String[] args) throws IOException {
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		P = N - K;
-		sb = new StringBuilder(P);
-		num = br.readLine();
-		idx = -1;
-		while (P > 0) {
-			tmp = '/'; // '0' 바로 직전.
-			for (int i = idx + 1; i <= N - P; i++) {
-				ch = num.charAt(i);
-				if (ch > tmp) {
-					tmp = ch;
-					idx = i;
-					if (ch == '9') {
-						break;
-					}
-				}
-			}
-			sb.append(tmp);
-			P--;
-		}
-		System.out.println(sb.toString());
-	}
+    static void input() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        inputNum = br.readLine();
+        P = N - K;
+        sb = new StringBuilder(P);
+        idx = -1;
+    }
+
+    static void solve() {
+        while (P > 0) { // P가 숫자가 들어갈수 있는 공간이라고 생각 -> 공간이 다 채워지면 종료
+            tmp = '/'; // '0' 바로 직전
+            int limit = N - P;
+            for (int i = idx + 1; i <= limit; i++) {
+                ch = inputNum.charAt(i);
+                if (ch > tmp) {
+                    tmp = ch;
+                    idx = i;
+                    if (ch == '9') {
+                        break;
+                    }
+                }
+            }
+            sb.append(tmp);
+            P--;
+        }
+    }
+
+    static void output() throws IOException {
+        bw.write(sb.toString());
+        bw.flush();
+    }
 }

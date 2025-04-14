503ac313581fb824255c3c93daf6664b450677df
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-04-15 00:13:57 +0900

    [Gold III] Title: 크게 만들기, Time: 144 ms, Memory: 18536 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.java
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.java
@@ -7,10 +7,10 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
-    static int N, K, P, idx; // N: 숫자 자리수, K: 소거할 숫자 개수, P: 남길 숫자 개수(N - K), idx: 마지막에 사용된 index
-    static String inputNum; // 입력할 숫자 String
-    static char ch, tmp; // ch: 문자(사실은 숫자), tmp: 비교할 임시값
-    static StringBuilder sb; // 정답
+    static int N, K, P;
+    static String inputNum;
+    static StringBuilder sb;
+
     public static void main(String[] args) throws IOException {
         input();
         solve();
@@ -22,27 +22,30 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
         inputNum = br.readLine();
+        // 남길 자릿수
         P = N - K;
-        sb = new StringBuilder(P);
-        idx = -1;
     }
 
     static void solve() {
-        while (P > 0) { // P가 숫자가 들어갈수 있는 공간이라고 생각 -> 공간이 다 채워지면 종료
-            tmp = '/'; // '0' 바로 직전
-            int limit = N - P;
-            for (int i = idx + 1; i <= limit; i++) {
-                ch = inputNum.charAt(i);
-                if (ch > tmp) {
-                    tmp = ch;
-                    idx = i;
-                    if (ch == '9') {
-                        break;
-                    }
-                }
+        // 모노토닉 스택을 배열 + top 인덱스로 구현
+        char[] stack = new char[N];
+        int top = -1;      // 스택의 top 인덱스
+        int toRemove = K;  // 제거해야 할 숫자 개수
+
+        for (int i = 0; i < N; i++) {
+            char c = inputNum.charAt(i);
+            // 스택이 비어있지 않고, 아직 제거 가능하며,
+            // top에 있는 문자보다 현재 문자가 더 크면 pop
+            while (top >= 0 && toRemove > 0 && stack[top] < c) {
+                top--;
+                toRemove--;
             }
-            sb.append(tmp);
-            P--;
+            stack[++top] = c; // 현재 문자 push
+        }
+        
+        sb = new StringBuilder(P);
+        for (int i = 0; i < P; i++) {
+            sb.append(stack[i]);
         }
     }
 

8b8111ccd4ef5affde32f4b78b3788fa5d0af477
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-04-15 23:26:02 +0900

    [level 1] Title: 모의고사, Time: 2.57 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,18 +1,18 @@
 def solution(answers):
     # 1번, 2번, 3번 수포자들
-    student1 = [1, 2, 3, 4, 5]
-    student2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    student3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    s1 = [1, 2, 3, 4, 5]
+    s2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    s3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
     answer = []
     score = [0, 0, 0]
     
     for i in range(len(answers)):
-        if answers[i] == student1[i % 5]:
+        if answers[i] == s1[i % 5]:
             score[0] += 1
-        if answers[i] == student2[i % 8]:
+        if answers[i] == s2[i % 8]:
             score[1] += 1
-        if answers[i] == student3[i % 10]:
+        if answers[i] == s3[i % 10]:
             score[2] += 1
     
     for idx, num in enumerate(score):

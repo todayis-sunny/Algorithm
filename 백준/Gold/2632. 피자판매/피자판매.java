65055706decfe5415d728fd20e97c87d422fd356
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-04-29 14:32:14 +0900

    [Gold II] Title: 피자판매, Time: 100 ms, Memory: 17476 KB -BaekjoonHub

--- 백준/Gold/2632. 피자판매/피자판매.java
+++ 백준/Gold/2632. 피자판매/피자판매.java
@@ -1,10 +1,7 @@
 // 02632. [G2] 피자판매.
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
-import java.util.TreeMap;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,7 +10,7 @@ public class Main {
     static int S, A, B; // S: 손님이 구매하고자 하는 피자 크기, A: A피자 조각 개수 B: B피자 조각 개수
     static int[] pizzaA, pizzaB; // 피자별 피자조각 크기
     static int[] prefixSumA, prefixSumB; // 각 누적합
-    static Map<Integer, Integer> setA, setB; // 가능한 크기 조합 개수
+    static int[] combA, combB; // 피자 크기별 가능한 개수
     static int ans = 0; // 방법의 가지 수
 
     public static void main(String[] args) throws IOException {
@@ -27,84 +24,65 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         A = Integer.parseInt(st.nextToken());
         B = Integer.parseInt(st.nextToken());
-        pizzaA = new int[A];
-        pizzaB = new int[B];
-        for (int i = 0; i < A; i++) {
-            pizzaA[i] = Integer.parseInt(br.readLine());
+        // (1-based)
+        pizzaA = new int[A * 2 + 1];
+        pizzaB = new int[B * 2 + 1];
+        for (int a = 1; a <= A; a++) {
+            pizzaA[a] = pizzaA[a + A] = Integer.parseInt(br.readLine());
         }
-        for (int i = 0; i < B; i++) {
-            pizzaB[i] = Integer.parseInt(br.readLine());
+        for (int b = 1; b <= B; b++) {
+            pizzaB[b] = pizzaB[b + B] = Integer.parseInt(br.readLine());
         }
     }
 
     static void solve() {
-        prefixSumA = new int[A];
-        prefixSumB = new int[B];
-        // A를 기준으로 오름차순 탐색
-        setA = new TreeMap<>();
-        setB = new HashMap<>();
-        // 0은 초기화
-        prefixSumA[0] = pizzaA[0];
-        prefixSumB[0] = pizzaB[0];
-        // 피자 크기를 0을 만드는 방법은 1개씩 존재 -> 아무것도 선택하지 않았음
-        setA.put(0, 1);
-        setB.put(0, 1);
         // 누적합 초기화
-        for (int a = 1; a < A; a++) {
+        prefixSumA = new int[A * 2 + 1];
+        prefixSumB = new int[B * 2 + 1];
+        for (int a = 1; a <= A * 2; a++) {
             prefixSumA[a] = prefixSumA[a - 1] + pizzaA[a];
         }
-        for (int b = 1; b < B; b++) {
+        for (int b = 1; b <= B * 2; b++) {
             prefixSumB[b] = prefixSumB[b - 1] + pizzaB[b];
         }
-        // 피자 크기를 맥스로 하는 방법은 1개씩 존재 -> 원형인거를 생각
-        setA.put(prefixSumA[A - 1], 1);
-        setB.put(prefixSumB[B - 1], 1);
-        // A피자 누적합 계산
-        for (int a = 0; a < A; a++) {
-            for (int plus = 0; plus < A - 1; plus++) {
-                int key = getPrefixSum(true, a, a + plus);
-                setA.put(key, setA.getOrDefault(key, 0) + 1);
+        // 콤비네이션 초기화
+        combA = new int[S + 1];
+        combB = new int[S + 1];
+        // 0은 선택하지 않은 경우니까 1개씩 존재
+        combA[0] = 1;
+        combB[0] = 1;
+        for (int al = 1; al < A; al++) { // al : 부분합의 길이
+            for (int a = 1; a <= A; a++) { // a :
+                int value = prefixSumA[a + al - 1] - prefixSumA[a - 1];
+                if (value > S) {
+                    continue;
+                }
+                combA[value]++;
             }
         }
-        // B피자 누적합 계산
-        for (int b = 0; b < B; b++) {
-            for (int plus = 0; plus < B - 1; plus++) {
-                int key = getPrefixSum(false, b, b + plus);
-                setB.put(key, setB.getOrDefault(key, 0) + 1);
+        if (prefixSumA[A] <= S) {
+            combA[prefixSumA[A]]++;
+        }
+        for (int bl = 1; bl < B; bl++) {
+            for (int b = 1; b <= B; b++) {
+                int value = prefixSumB[b + bl - 1] - prefixSumB[b - 1];
+                if (value > S) {
+                    continue;
+                }
+                combB[value]++;
             }
         }
-        for (int aKey : setA.keySet()) {
-            if (aKey > S) {
-                break;
-            }
-            int bKey = S - aKey;
-            ans += setA.get(aKey) * setB.getOrDefault(bKey, 0);
+        if (prefixSumB[B] <= S) {
+            combB[prefixSumB[B]]++;
+        }
+        // 정답구하기
+        for (int s = 0; s <= S; s++) { // A피자에서 s크기 선택시 B피자는 S - s 선택
+            ans += combA[s] * combB[S - s];
         }
     }
 
     static void output() throws IOException {
         bw.write(String.valueOf(ans));
         bw.flush();
-    }
-
-    static int getPrefixSum(boolean isA, int start, int end) {
-        int[] target;
-        int size;
-        if (isA) {
-            target = prefixSumA;
-            size = A;
-        } else {
-            target = prefixSumB;
-            size = B;
-        }
-        if (end > target.length - 1) {
-            return target[size - 1] - target[start - 1] + target[end % size];
-        } else if (start == 0 && end == 0) {
-            return target[0];
-        } else if (start == 0) {
-            return target[end];
-        } else {
-            return target[end] - target[start - 1];
-        }
     }
 }

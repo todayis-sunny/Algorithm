1b977e20bc9d577278a121c2823dac303b7220da
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-05-19 22:34:08 +0900

    [level 2] Title: 짝지어 제거하기, Time: 109.24 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,11 +1,15 @@
 def solution(s):
     stack = []
     for letter in s:
+        # 스택이 비어 있으면 추가
         if not stack:
             stack.append(letter)
+        # 스택이 비어 있지 않고
         else:
+            # 짝이면 제거
             if stack[-1] == letter:
                 stack.pop()
+            # 짝이 아니면 추가
             else:
                 stack.append(letter)
                 

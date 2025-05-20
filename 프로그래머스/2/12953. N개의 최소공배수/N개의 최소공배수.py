cdec234f4d80df0bad26894655ad420e43631da8
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-05-20 23:35:20 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.00 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,7 +1,7 @@
 from math import gcd 
 
 def solution(arr):
-                               
+    # 가장 작은 수를 가져옴
     ans = arr[0]                                 
     for num in arr:                           
         ans = ans * num // gcd(ans, num)     

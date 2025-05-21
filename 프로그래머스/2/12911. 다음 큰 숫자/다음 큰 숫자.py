407b59d9ffb529a872adcb23da7e3ac83de39169
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-05-21 23:28:07 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,11 +1,11 @@
 def solution(n):
-    next = n
+    num = n
     answer = 0
     
     while True:
-        next += 1
-        if bin(n)[2:].count('1') == bin(next)[2:].count('1'):
-            answer = next
+        num += 1
+        if bin(n)[2:].count('1') == bin(num)[2:].count('1'):
+            answer = num
             break
     
     return answer

1055ddedacb3a5d3be4c933f6367001f667503a5
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-05-28 17:15:30 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.62 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,7 +1,8 @@
 def solution(n):
+    DIV = 1_234_567
     dp = [1] + [0] * n
     dp[0] = 1
     dp[1] = 1
     for i in range(2, n + 1):
-        dp[i] = (dp[i-1] + dp[i-2]) % 1_234_567
+        dp[i] = (dp[i-1] + dp[i-2]) % DIV
     return dp[n]

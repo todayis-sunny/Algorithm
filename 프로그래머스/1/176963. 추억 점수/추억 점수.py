8533deda459ccbc424f72b6d43401d570bdbe396
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-05-30 23:22:07 +0900

    [level 1] Title: 추억 점수, Time: 17.50 ms, Memory: 9.91 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -2,9 +2,9 @@ def solution(name, yearning, photo):
     ans = []
     for p in photo:
         score = 0
-        for n in p:
-            if n in name:
-                score += yearning[name.index(n)]
+        for k in p:
+            if k in name:
+                score += yearning[name.index(k)]
         ans.append(score)
 
     return ans

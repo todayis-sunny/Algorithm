7452e9733dfef528c5b66fb486a0358efb332b5a
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-07-04 17:10:09 +0900

    [level 2] Title: 더 맵게, Time: 1950.02 ms, Memory: 67 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,13 +1,13 @@
-import heapq
+from heapq import heappop, heappush
 
 def solution(S, K):
     heap = []
     for i in S:
-        heapq.heappush(heap, i)
+        heappush(heap, i)
 
     cnt = 0
     while heap[0] < K:
-        heapq.heappush(heap, heapq.heappop(heap) + heapq.heappop(heap) * 2)
+        heappush(heap, heappop(heap) + heappop(heap) * 2)
         cnt += 1
         
         if len(heap) == 1 and heap[0] < K:

ac3915fe61aee58b9855a9629c64a1b6e0e73a99
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-08-04 20:48:47 +0900

    [level 2] Title: 방문 길이, Time: 0.36 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,10 +1,10 @@
 def solution(dirs):
     sets = set()
     y, x = 0, 0
-    udrl = {'U': (1, 0), 'D': (-1, 0), 'R': (0, 1), 'L': (0, -1)}
+    URDL = {'U': (1, 0), 'D': (-1, 0), 'R': (0, 1), 'L': (0, -1)}
     
     for d in dirs:
-        dy, dx = udrl[d]
+        dy, dx = URDL[d]
         ny = y + dy
         nx = x + dx
         if -5 <= ny <= 5 and -5 <= nx <= 5:

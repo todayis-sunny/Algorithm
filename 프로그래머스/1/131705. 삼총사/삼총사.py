875aafe5a95bd03732eb5c0138ba2fcae3244a7b
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-08-20 18:11:25 +0900

    [level 1] Title: 삼총사, Time: 0.07 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -3,7 +3,7 @@ from itertools import combinations
 def solution(number):
     arr = list(combinations(number, 3))
     ans = 0
-    for element in arr:
-        if sum(element) == 0:
+    for el in arr:
+        if sum(el) == 0:
             ans += 1
     return ans

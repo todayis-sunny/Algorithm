5146c3f22fe93e3d9be798e6975393ba62a6d077
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-09-06 19:28:08 +0900

    [Platinum I] Title: 아파트 임대, Time: 1992 ms, Memory: 307124 KB -BaekjoonHub

--- 백준/Platinum/5615. 아파트 임대/아파트 임대.java
+++ 백준/Platinum/5615. 아파트 임대/아파트 임대.java
@@ -15,24 +15,12 @@ public class Main {
     static long[] kValues; // 입력받은 k 값들을 저장할 배열
     static int ans = 0;    // 최종 정답
 
-    // 밀러-라빈 테스트에 사용할 밑수들
-    static final BigInteger[] PRIME_BASES = {
-            new BigInteger("2"), new BigInteger("3"), new BigInteger("5"),
-            new BigInteger("7"), new BigInteger("11")
-    };
-
-    // 자주 사용하는 BigInteger 상수들
-    static final BigInteger ZERO = BigInteger.ZERO;
-    static final BigInteger ONE = BigInteger.ONE;
-    static final BigInteger TWO = new BigInteger("2");
-
     public static void main(String[] args) throws IOException {
         input();
         solve();
         output();
     }
 
-    /** 입력 처리 */
     static void input() throws IOException {
         N = Integer.parseInt(br.readLine());
         kValues = new long[N];
@@ -41,69 +29,17 @@ public class Main {
         }
     }
 
-    /** 핵심 로직 처리 */
     static void solve() {
-        for (long k : kValues) {
-            BigInteger n = BigInteger.valueOf(k).multiply(TWO).add(ONE);
-
-            // 2보다 작은 수는 소수가 아님
-            if (n.compareTo(TWO) < 0) {
-                continue;
-            }
-
-            boolean isPrime = true;
-            for (BigInteger base : PRIME_BASES) {
-                // n이 밑수와 같으면 소수
-                if (n.equals(base)) {
-                    isPrime = true;
-                    break;
-                }
-                // 밀러-라빈 테스트 실패 시
-                if (!millerRabin(n, base)) {
-                    isPrime = false;
-                    break;
-                }
-            }
-
-            if (isPrime) {
+        for (long value : kValues) {
+            long k = value * 2 + 1;
+            if (new BigInteger(Long.toString(k)).isProbablePrime(10)) {
                 ans++;
             }
         }
     }
 
-    /** 출력 처리 */
     static void output() throws IOException {
         bw.write(String.valueOf(ans));
         bw.flush();
-        bw.close();
-    }
-
-    /**
-     * 밀러-라빈 소수 판별법 (BigInteger 버전)
-     */
-    static boolean millerRabin(BigInteger n, BigInteger a) {
-        BigInteger r = ZERO;
-        BigInteger d = n.subtract(ONE); // d = n - 1
-
-        // d가 짝수인 동안 (d % 2 == 0)
-        while (!d.testBit(0)) {
-            r = r.add(ONE);      // r += 1
-            d = d.shiftRight(1); // d /= 2
-        }
-
-        BigInteger x = a.modPow(d, n); // x = powmod(a, d, n)
-
-        if (x.equals(ONE) || x.equals(n.subtract(ONE))) {
-            return true;
-        }
-
-        // for (int i = 0; i < r - 1; i++)
-        for (BigInteger i = ZERO; i.compareTo(r.subtract(ONE)) < 0; i = i.add(ONE)) {
-            x = x.modPow(TWO, n); // x = powmod(x, 2, n)
-            if (x.equals(n.subtract(ONE))) {
-                return true;
-            }
-        }
-        return false;
     }
 }

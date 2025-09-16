21e847dc8e052338eeea492bb2b33ddf2a855ec9
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-09-16 17:22:12 +0900

    [level 2] Title: 요격 시스템, Time: 425.33 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -1,9 +1,9 @@
 def solution(targets):
-    answer = 0
-    targets.sort(key=lambda x: x[1])
+    ans = 0
+    targets.sort(key = lambda x: x[1])
     e = 0
     for tg in targets:
         if tg[0] >= e:
-            answer += 1
+            ans += 1
             e = tg[1]
-    return answer
+    return ans

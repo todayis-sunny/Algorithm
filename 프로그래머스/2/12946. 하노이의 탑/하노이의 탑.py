bf88b89104b0650f698d498268ebca4646367e5a
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-09-19 23:11:08 +0900

    [level 2] Title: 하노이의 탑, Time: 10.46 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -1,14 +1,14 @@
 def solution(n):
-    answer = []
+    ans = []
 
     def hanoi(src, tgt, inter, n):
         if n == 1:
-            answer.append([src, tgt])
+            ans.append([src, tgt])
         else:
-            hanoi(src,inter,tgt,n-1)
-            hanoi(src,tgt,inter,1)
-            hanoi(inter,tgt,src,n-1)
+            hanoi(src, inter, tgt, n-1)
+            hanoi(src, tgt, inter,1)
+            hanoi(inter, tgt, src, n-1)
             
     hanoi(1, 3, 2, n)
     
-    return answer
+    return ans

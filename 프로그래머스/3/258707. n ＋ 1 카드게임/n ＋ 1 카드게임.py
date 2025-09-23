b761d15645402346d06c468f07c8ca13e5ba6ed1
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-09-23 17:26:06 +0900

    [level 3] Title: n + 1 카드게임, Time: 0.65 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.py
@@ -0,0 +1,54 @@
+import heapq
+
+def solution(coin, cards):
+    length = len(cards)
+    goal = length + 1
+    freeIdx = length // 3 - 1
+    
+    idxArr = [0] * (length + 1)
+    for i in range(length):
+        idxArr[cards[i]] = i
+        
+    hq = []
+    # 전처리
+    for j in range(freeIdx + 1):
+        if idxArr[goal - cards[j]] <= j:
+            heapq.heappush(hq, 0)
+    
+    
+    # 라운드
+    answer = 0
+    # 현재까지의 인덱스
+    curr = freeIdx
+    # 남은 코인
+    coins = coin
+    # 인덱스가 마지막까지 도달할때 까지 반복
+    while True:
+        answer += 1
+        if curr == length-1:
+            break
+        card1 = cards[curr+1]
+        card2 = cards[curr+2]
+        
+        curr += 2
+        # card1에 대한 처리.
+        if freeIdx < idxArr[goal - card1] < idxArr[card1]:
+            heapq.heappush(hq, 2)
+        elif idxArr[goal - card1] <= freeIdx:
+            heapq.heappush(hq, 1)
+        # card2에 대한 처리. 
+        if freeIdx < idxArr[goal - card2] < idxArr[card2]:
+            heapq.heappush(hq, 2)
+        elif idxArr[goal - card2] <= freeIdx:
+            heapq.heappush(hq, 1)
+        
+        # 뺄게 없으면 break
+        if not hq:
+            break
+        cost = heapq.heappop(hq)
+        coins -= cost
+        # coin이 부족해지면 break
+        if coins < 0:
+            break
+    
+    return answer

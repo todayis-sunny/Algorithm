4258937898175daec81f08e5cc0bf62b037a86cf
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-10-12 21:00:38 +0900

    [level 1] Title: 기사단원의 무기, Time: 1300.03 ms, Memory: 9.46 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,9 +1,9 @@
 def solution(number, limit, power):
-    answer = 0
+    ans = 0
     
     for i in range(1, number+1):
         temp = 0
-        for j in range(1,int(i**0.5)+1):
+        for j in range(1, int(i ** 0.5) + 1):
             if i % j == 0:
                 if i != j*j:
                     temp += 2
@@ -12,6 +12,6 @@ def solution(number, limit, power):
             if temp > limit:
                 temp = power
                 break
-        answer += temp
+        ans += temp
         
-    return answer
+    return ans

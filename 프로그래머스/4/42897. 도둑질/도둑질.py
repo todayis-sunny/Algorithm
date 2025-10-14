f1a7127bfc39f6d4c783658b85269345df963f46
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-10-14 16:06:06 +0900

    [level 4] Title: 도둑질, Time: 561.42 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.py
+++ 프로그래머스/4/42897. 도둑질/도둑질.py
@@ -6,7 +6,6 @@ def solution(money):
     for i in range(1, len(money) - 1):  # 마지막 집은 털지 못함
         dp1[i] = max(dp1[i - 1], dp1[i - 2] + money[i])
     # 1번 집을 안터는 경우
-    dp1[0] = 0
     for i in range(1, len(money)):
         dp2[i] = max(dp2[i - 1], dp2[i - 2] + money[i])
 

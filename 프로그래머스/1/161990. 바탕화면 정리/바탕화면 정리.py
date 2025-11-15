232f2858598076ac9f3d1b91ada865379775a336
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-11-15 18:32:02 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.48 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,16 +1,16 @@
 def solution(wallpaper):
-    answer = [-1, 50, -1, -1]
+    result = [-1, 50, -1, -1]
     
     for i in range(len(wallpaper)):
         if '#' in wallpaper[i]:
-            if answer [0] == -1:
-                answer[0] = i
-            answer[2] = i + 1
+            if result [0] == -1:
+                result[0] = i
+            result[2] = i + 1
         for j in range(len(wallpaper[0])):
             if wallpaper[i][j] == '#':
-                if j < answer[1]:
-                    answer[1] = j
-                if answer[3] < j + 1:
-                    answer[3] = j + 1
+                if j < result[1]:
+                    result[1] = j
+                if result[3] < j + 1:
+                    result[3] = j + 1
 
-    return answer
+    return result

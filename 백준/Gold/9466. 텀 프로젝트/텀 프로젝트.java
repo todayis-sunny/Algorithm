f03d4e621c0a322b99aefa013306f18899a041c9
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-11-27 02:46:33 +0900

    [Gold III] Title: 텀 프로젝트, Time: 920 ms, Memory: 298260 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -8,10 +8,9 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
-    static int TC, N, ans;
-    static final int UNKNOW = 0, FINDING = 1, MATCHED = 2, SOLO = -1;
+    static int TC, N, cnt;
     static int[] wanted; // 1-based
-    static int[] state;
+    static boolean[] visited, finished;
     public static void main(String[] args) throws IOException {
         input();
         output();
@@ -23,23 +22,21 @@ public class Main {
             N = Integer.parseInt(br.readLine());
             st = new StringTokenizer(br.readLine());
             wanted = new int[N + 1];
-            state = new int[N + 1];
+            visited = new boolean[N + 1];
+            finished = new boolean[N + 1];
             for (int n = 1; n <= N; n++) {
                 wanted[n] = Integer.parseInt(st.nextToken());
             }
-            ans = 0;
+            cnt = 0;
             solve();
         }
     }
 
     static void solve() {
         for (int n = 1; n <= N; n++) {
-            if (state[n] == UNKNOW) dfs(n);
+            dfs(n);
         }
-        for (int n = 1; n <= N; n++) {
-            if (state[n] == SOLO) ans++;
-        }
-        sb.append(ans).append("\n");
+        sb.append(N - cnt).append("\n");
 
     }
 
@@ -47,26 +44,24 @@ public class Main {
         bw.write(sb.toString());
         bw.flush();
     }
-    static void dfs(int student) {
-        // 학생이 팀을 탐색중이면 루프 종료
-        if (state[student] == FINDING) {
-            order(student, MATCHED);
-            return;
-        }
-        // 희망하는 학생이 이미 매칭된 팀이 있거나, 솔로라면 루프 종료
-        if (state[student] == MATCHED || state[student] == SOLO) {
-            return;
-        }
-        state[student] = FINDING;
-        dfs(wanted[student]);
-        if (state[student] != MATCHED) {
-            state[student] = SOLO;
-        }
-    }
+    static void dfs(int curr) {
+        if (visited[curr]) return;
 
-    static void order(int student, int status) {
-        if (state[student] == status) return;
-        state[student] = status;
-        order(wanted[student], status);
+        visited[curr] = true;
+        int next = wanted[curr];
+        // 학생이 탐색되지 않은 경우
+        if (!visited[next]) {
+            dfs(next);
+        } else {
+            if (!finished[next]) {
+                // 노드가 종료되려면 싸이클을 무조건 거침
+                cnt++;
+                while (next != curr) {
+                    cnt++;
+                    next = wanted[next];
+                }
+            }
+        }
+        finished[curr] = true;
     }
 }

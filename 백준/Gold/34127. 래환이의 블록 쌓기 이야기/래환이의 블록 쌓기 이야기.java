5c4bbd5d57a95a2a6b4fc58aa8bb2395a47b5b4f
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-11-29 21:12:28 +0900

    [Gold III] Title: 래환이의 블록 쌓기 이야기, Time: 292 ms, Memory: 38860 KB -BaekjoonHub

--- 백준/Gold/34127. 래환이의 블록 쌓기 이야기/래환이의 블록 쌓기 이야기.java
+++ 백준/Gold/34127. 래환이의 블록 쌓기 이야기/래환이의 블록 쌓기 이야기.java
@@ -1,3 +1,5 @@
+// 34127. [G3] 래환이의 블록 쌓기 이야기.
+
 import java.io.*;
 import java.util.StringTokenizer;
 
@@ -31,78 +33,16 @@ public class Main {
     static void solve() {
         // 높이가 이미 오름차순인 경우 체크
         long prev = -1; // 이전 높이 기준치 : 1-based이므로 0번 인덱스는 0 죽 -1부터 하면 선형적
-        keep:
-        for (; true; ) {
-            for (int height : building) {
-                // 오름차순이 아닌 경우 탈출
-                if (height <= prev) break keep;
-                prev = height;
-            }
-            return;
-        }
-        // 높이가 오름차순이 아닌 경우
         long total = 0; // 변화량
-        for (int i = 1; i <= N - 1; i++) {
+        for (int i = 1; i <= N; i++) {
             prev = building[i - 1] + result[i - 1];
-            // 빌딩이 이미 큰 경우
-            if (building[i] > prev) {
-                // 1차이인 경우 스킵
-                if (building[i] == prev + 1) continue;
-                    // 2차이인 경우
-                else if (building[i] == prev + 2) {
-                    // 짝수만큼 떼줄수 있는 경우
-                    if (i % 2 == 0) {
-                        continue; // 떼줄수 없음을 의미
-                    }
-                    // 홀수만큼 떼줄수 있는 경우
-                    else {
-                        total -= 1;
-                        result[i] = -1;
-                    }
-                }
-                // 3차이 이상인 경우
-                else {
-                    // prev + 1
-                    long increment = (prev + 1) - building[i];
-                    // (짝 인덱스, 짝 증감) or (홀 인덱스 , 홀 증감) : xor 연산하여 0번째 비트가 0이면
-                    if (((i ^ Math.abs(increment)) & 1) == 0) {
-                        total += increment;
-                        result[i] = increment;
-                    }
-                    // prev + 2
-                    else {
-                        total += increment + 1;
-                        result[i] = increment + 1;
-                    }
-
-                }
-            }
-            // 빌딩이 작은 경우
-            else {
-                long increment = (prev + 1) - building[i];
-                // (짝 인덱스, 짝 증감) or (홀 인덱스 , 홀 증감) : xor 연산하여 0번째 비트가 0이면
-                if (((i ^ Math.abs(increment)) & 1) == 0) {
-                    total += increment;
-                    result[i] = increment;
-                }
-                // prev + 2
-                else {
-                    total += increment + 1;
-                    result[i] = increment + 1;
-                }
-            }
+            long increment = (prev + 1) - building[i];
+            if (increment == 0) continue;
+            // (짝 인덱스, 짝 증감) or (홀 인덱스 , 홀 증감) : xor 연산하여 0번째 비트가 0이면
+            total += result[i] = increment + ((i ^ Math.abs(increment)) & 1);
         }
         // 마지막 건물에 증감 적용
-        prev = building[N - 1] + result[N - 1];
-        // (짝 인덱스, 짝 증감) or (홀 인덱스 , 홀 증감) : xor 연산하여 0번째 비트가 0이면
-        if (total == 0) {
-           result[N] = 0;
-        }
-        else if (((N ^ Math.abs(total)) & 1) == 0) {
-            result[N] = (-total);
-        } else {
-            result[N] = (-(total + 1));
-        }
+        result[N] += -((total / 2) * 2);
         if (building[N] + result[N] > prev) return;
         possible = false;
     }

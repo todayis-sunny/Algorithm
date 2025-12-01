b83f2701c8458f63408dbf2be395c52e8f6b2a51
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-12-02 00:48:22 +0900

    [Gold V] Title: 경쟁적 전염, Time: 192 ms, Memory: 21112 KB -BaekjoonHub

---
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
@@ -0,0 +1,100 @@
+// 18405. [G5] 경쟁적 전염.
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int N, K, S; // N: 배열의 크기, K: 바이러스 종류, S: 제한시간
+    static int[][] testTube; // 시험관
+    static final int[] dx = {-1, 1, 0, 0};
+    static final int[] dy = {0, 0, -1, 1};
+    static List<List<Node>> virusList = new ArrayList<>(); // 1-based;
+    static Node find;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solve();
+        output();
+    }
+
+    static void input() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        // 바이러스 리스트 초기화
+        for (int k = 0; k <= K; k++) {
+            virusList.add(new ArrayList<>());
+        }
+
+        // 시험관 정보 입력
+        testTube = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                int v = Integer.parseInt(st.nextToken());
+                testTube[i][j] = v;
+                virusList.get(v).add(new Node(i, j));
+            }
+        }
+        // 제한시간과 위치 입력
+        st = new StringTokenizer(br.readLine());
+        S = Integer.parseInt(st.nextToken());
+        // 찾는 위치는 1-based이므로 우리의 조건에 맞게 0-based
+        find = new Node(Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1);
+    }
+
+    static void solve() {
+        bfs();
+    }
+
+    static void output() throws IOException {
+        bw.write(String.valueOf(testTube[find.x][find.y]));
+        bw.flush();
+    }
+
+    static void bfs() {
+        Queue<Node> queue = new LinkedList<>();
+        // 큐에 사전 작업
+        for (int k = 1; k <= K; k++) {
+            for (Node node : virusList.get(k)) {
+                queue.offer(node);
+            }
+        }
+        // 제한시간까지 계산
+        while (!queue.isEmpty()) {
+            Node curr = queue.poll();
+            // 제한시간이 초과되면 종료
+            if (curr.time >= S) return;
+            for (int i = 0; i < 4; i++) {
+                int nx = curr.x + dx[i];
+                int ny = curr.y + dy[i];
+                // 범위 밖이면 스킵
+                if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+                // 이미 감염됐으면 스킵
+                if (testTube[nx][ny] != 0) continue;
+                // 위에 조건을 통과하면 감염
+                testTube[nx][ny] = testTube[curr.x][curr.y]; // 숙주로 부터 새 위치 감염
+                queue.offer(new Node(nx, ny, curr.time + 1)); // 새로운 바이러스 생성
+            }
+        }
+    }
+
+    static class Node {
+        int x, y;
+        int time = 0;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Node(int x, int y, int time) {
+            this.x = x;
+            this.y = y;
+            this.time = time;
+        }
+    }
+}

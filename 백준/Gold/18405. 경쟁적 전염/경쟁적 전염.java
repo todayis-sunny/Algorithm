f17a3e66077824742bff6d41f3085f5ebc910496
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-12-02 01:54:25 +0900

    [Gold V] Title: 경쟁적 전염, Time: 128 ms, Memory: 16968 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
@@ -11,8 +11,9 @@ public class Main {
     static int[][] testTube; // 시험관
     static final int[] dx = {-1, 1, 0, 0};
     static final int[] dy = {0, 0, -1, 1};
-    static List<List<Node>> virusList = new ArrayList<>(); // 1-based;
+    static boolean[][] visited;
     static Node find;
+    static int ans = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         input();
@@ -24,24 +25,19 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        // 바이러스 리스트 초기화
-        for (int k = 0; k <= K; k++) {
-            virusList.add(new ArrayList<>());
-        }
-
         // 시험관 정보 입력
         testTube = new int[N][N];
+        visited = new boolean[N][N];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 int v = Integer.parseInt(st.nextToken());
                 testTube[i][j] = v;
-                virusList.get(v).add(new Node(i, j));
             }
         }
         // 제한시간과 위치 입력
         st = new StringTokenizer(br.readLine());
-        S = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken()) + 1;
         // 찾는 위치는 1-based이므로 우리의 조건에 맞게 0-based
         find = new Node(Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1);
     }
@@ -51,33 +47,43 @@ public class Main {
     }
 
     static void output() throws IOException {
-        bw.write(String.valueOf(testTube[find.x][find.y]));
+        if (ans == Integer.MAX_VALUE) ans = 0;
+        bw.write(String.valueOf(ans));
         bw.flush();
     }
 
     static void bfs() {
         Queue<Node> queue = new LinkedList<>();
-        // 큐에 사전 작업
-        for (int k = 1; k <= K; k++) {
-            for (Node node : virusList.get(k)) {
-                queue.offer(node);
-            }
-        }
+        // 이미 감염되어 있으면 종료
+        if (testTube[find.x][find.y] != 0) {
+            ans = testTube[find.x][find.y];
+            return;
+        };
+        queue.offer(new Node(find.x, find.y));
+        visited[find.x][find.y] = true;
+
+
         // 제한시간까지 계산
         while (!queue.isEmpty()) {
             Node curr = queue.poll();
             // 제한시간이 초과되면 종료
             if (curr.time >= S) return;
+            // 이미 찾은 경우 값 갱신
+            if (testTube[curr.x][curr.y] < ans && testTube[curr.x][curr.y] != 0) {
+                S = curr.time + 1;
+                ans = testTube[curr.x][curr.y];
+            }
+
             for (int i = 0; i < 4; i++) {
                 int nx = curr.x + dx[i];
                 int ny = curr.y + dy[i];
                 // 범위 밖이면 스킵
                 if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-                // 이미 감염됐으면 스킵
-                if (testTube[nx][ny] != 0) continue;
+                // 이미 방문했으면 스킵
+                if (visited[nx][ny]) continue;
                 // 위에 조건을 통과하면 감염
-                testTube[nx][ny] = testTube[curr.x][curr.y]; // 숙주로 부터 새 위치 감염
-                queue.offer(new Node(nx, ny, curr.time + 1)); // 새로운 바이러스 생성
+                queue.offer(new Node(nx, ny, curr.time + 1)); // 새로운 탐색
+                visited[nx][ny] = true;
             }
         }
     }

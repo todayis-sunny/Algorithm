75914792c60a044f16cf5ef7eaedf00478c29904
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-12-15 23:31:49 +0900

    [level 3] Title: 기지국 설치, Time: 2.23 ms, Memory: 9.56 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -1,17 +1,16 @@
+import math
+
 def solution(n, stations, w):
-    answer = 0
+    result = 0
     wave = w * 2 + 1 # 파동
     start = 1
     for s in stations:
         if s - w - start > 0:
-            div, mod = divmod((s - w - start), wave)
-            answer += div
-            if mod: # 파동이 나누어 떨어지지 않는다면
-                answer += 1
+            result += math.ceil((s - w - start) / wave)
         start = s + w + 1 # start 지점 변경
+        
     if n - start + 1 > 0:
-        div, mod = divmod((n - start + 1), wave)
-        answer += div
-        if mod: # 파동이 나누어 떨어지지 않는다면
-            answer += 1
-    return answer
+        result += math.ceil((n - start + 1) / wave)
+    return result
+
+# 파동을 나누고 올림처리한다. -> 그만큼 기지국을 설치해야 함.

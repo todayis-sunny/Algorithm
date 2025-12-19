cb9a27e869f9a92d7b1be3fbf14bf6b4278f3a62
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-12-19 21:53:05 +0900

    [Gold V] Title: 배, Time: 108 ms, Memory: 15160 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.java
+++ 백준/Gold/1092. 배/배.java
@@ -11,6 +11,7 @@ public class Main {
     static int N, M;
     static int[] cranes, boxs;
     static int ans = 0;
+
     public static void main(String[] args) throws IOException {
         input();
         solve();
@@ -35,24 +36,23 @@ public class Main {
     static void solve() {
         Arrays.sort(cranes);
         Arrays.sort(boxs);
+
         if (boxs[M - 1] > cranes[N - 1]) {
             ans = -1;
             return;
         }
-        int remain = M;
-        while (remain > 0) {
-            craneLoop:
-            for (int i = N - 1; i >= 0; i--) {
-                if (remain == 0) break;
-                for (int j = M - 1; j >= 0; j--) {
-                    if (boxs[j] != 0 && boxs[j] <= cranes[i]) {
-                        boxs[j] = 0;
-                        remain--;
-                        continue craneLoop;
-                    }
-                }
+        ans = (M + N - 1) / N; // 올림 처리
+        int c = N;
+        int b = 0;
+        for (int i = 0; i < N - 1; i++) {
+            // 박스를 들수 있을때까지 들기
+            while (b < M && boxs[b] <= cranes[i]) {
+                b++;
             }
-            ans++;
+            // 크레인 1개 사용
+            c--;
+            // (남은 박스 수 + 남은 크레인 수 - 1) / 남은 크레인 수 : 남은 크레인들이 박스를 옮기는데 걸리는 최소 시간
+            ans = Math.max(ans , ((M - b) + c - 1) / c);
         }
     }
 
@@ -61,3 +61,8 @@ public class Main {
         bw.flush();
     }
 }
+
+/*
+ 풀이날짜 2025. 12. 18
+ 소요시간 00h 30m
+ */

1db72cd8c941bf27b05409518d7d5d5b0bcc5c6e
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2025-12-26 23:05:00 +0900

    [level 3] Title: 정수 삼각형, Time: 53.21 ms, Memory: 13.7 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,23 +1,22 @@
-def solution(triangle):    
-    
-    def memozation(row, col):
-        left, right = -1, -1
-        l = col - 1
-        r = col
-        if col == 0:
-            triangle[row][col] += triangle[row - 1][col]
-        elif col == row:
-            triangle[row][col] += triangle[row - 1][col - 1]
-        else:
-            triangle[row][col] += max(triangle[row - 1][col], triangle[row - 1][col - 1])
-    
-    
+
+def memozation(triangle, row, col):
+    left, right = -1, -1
+    l = col - 1
+    r = col
+    if col == 0:
+        triangle[row][col] += triangle[row - 1][col]
+    elif col == row:
+        triangle[row][col] += triangle[row - 1][col - 1]
+    else:
+        triangle[row][col] += max(triangle[row - 1][col], triangle[row - 1][col - 1])
+
+def solution(triangle):
     length = len(triangle)
     if length == 1:
         return triangle[0][0]
     
     for row in range(1, length):
         for col in range(row+1):
-            memozation(row, col)
+            memozation(triangle, row, col)
     return max(triangle[-1])
 

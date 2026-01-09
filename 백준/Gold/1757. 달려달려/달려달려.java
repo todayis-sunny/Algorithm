39034bf8db222e36fa80d6309c8276f75425bd07
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2026-01-10 02:28:32 +0900

    [Gold IV] Title: 달려달려, Time: 140 ms, Memory: 13524 KB -BaekjoonHub

--- 백준/Gold/1757. 달려달려/달려달려.java
+++ 백준/Gold/1757. 달려달려/달려달려.java
@@ -1,6 +1,7 @@
 // 01757. [G4] 달려달려.
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -8,9 +9,8 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
     static int N, M;
-    static int[] distance;
-    // dp[m][n] = m: 지침 지수, n: n분(현재 시간)
-    static int[][] dp; // [1-based][1-based]
+    static int[] prefixSum; // 1-based [i] : i분까지의 누적합(누적거리)
+    static int[] dp; // 1-based, [i] : i분에 지침 지수가 정확이 0인 상태에서 가질수 있는 최대 이동 거리
 
     public static void main(String[] args) throws IOException {
         input();
@@ -22,44 +22,39 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        distance = new int[N + 1];
-        dp = new int[M + 1][N + 1];
+        prefixSum = new int[N + 1];
+        dp = new int[N + 1];
         for (int n = 1; n <= N; n++) {
-            distance[n] = Integer.parseInt(br.readLine());
+            prefixSum[n] = prefixSum[n - 1] + Integer.parseInt(br.readLine());
         }
     }
 
 
     static void solve() {
-        // 학생들은 쉬기 시작하면 지침지수가 0이 되기 전에는 달릴수 없다.
-
-
-        for (int n = 1; n <= N; n++) {
-            dp[0][n] = dp[0][n - 1];
-            int limit = Math.min(M, n);
-            for (int m = 1; m <= limit; m++) {
-                // 달리는 경우
-                dp[m][n] = dp[m - 1][n - 1] + distance[n];
-                // 쭉 쉬는 경우 (지침지수)(현재시간 - 지침지수)
-                dp[0][n] = Math.max(dp[0][n], dp[m][n - m]);
+        for (int i = 0; i < N; i++) {
+            // 최대 달릴수 있는시간  min(지칠때까지, 현재남은시간/2)
+            int limit = Math.min(M, (N - i) / 2);
+            // 현재 i분일때 j분만큼 달리기
+            for (int j = 1; j <= limit; j++) {
+                // j분을 달리면 j분을 휴식, k = 2j만큼 소요
+                int k = j * 2; 
+                // i+1분 ~ j분까지 달리기
+                dp[i + k] = Math.max(dp[i + k], dp[i] + (prefixSum[i + j] - prefixSum[i]));
             }
+            // 그냥 휴식한번 하기
+            dp[i + 1] = Math.max(dp[i], dp[i + 1]);
         }
     }
 
     static void output() throws IOException {
-        bw.write(String.valueOf(dp[0][N]));
+        bw.write(String.valueOf(dp[N]));
         bw.flush();
-//        printDebug();
-    }
-
-    static void printDebug() {
-        System.out.println();
-        for (int m = 0; m <= M; m++) {
-            System.out.printf("m%d :", m);
-            for (int n = 0; n <= N; n++) {
-                System.out.printf("%2d ", dp[m][n]);
-            }
-            System.out.println();
-        }
     }
 }
+
+/*
+ # 풀이
+ - 누적합
+ - 1차원 DP
+
+ */

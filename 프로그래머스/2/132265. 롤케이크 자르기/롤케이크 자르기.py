c2d3eb2ad1ec3da92d5934e2ef9e199a6b2a31cb
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2026-01-22 16:19:27 +0900

    [level 2] Title: 롤케이크 자르기, Time: 623.49 ms, Memory: 49.7 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -3,12 +3,12 @@ from collections import Counter
 def solution(topping):
     dic = Counter(topping)
     setDic = set()
-    res = 0
+    result = 0
     for i in topping:
         dic[i] -= 1
         setDic.add(i)
         if dic[i] == 0:
             dic.pop(i)
         if len(dic) == len(setDic):
-            res += 1
-    return res
+            result += 1
+    return result

b00b021d7ff62f6c34221ed36650bf28b4cdbe04
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2026-03-06 18:31:34 +0900

    [level 1] Title: 노란불 신호등, Time: 140.58 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.py
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.py
@@ -1,3 +1,5 @@
+from math import gcd
+
 def solution(signals):
     # 신호정보
     info = []
@@ -6,8 +8,8 @@ def solution(signals):
         info.append(Signal(sig))
     # 최대 1주기 계산
     limit = 1
-    for data in info:
-        limit *= data.cycle
+    for x in info:
+        limit = lcm(limit, x.cycle)
     # 1주기에 대한 노란 신호 체크 여부
     for t in range(limit):
         for x in info:
@@ -21,13 +23,17 @@ def solution(signals):
             return (t + 1) # 0초 시작 -> 1초 시작
     return -1
             
-
+def lcm(a, b):
+    return (a * b) // gcd(a, b)
+    
 class Signal:
     def __init__(self, sig):
         self.cycle = sum(sig) # 주기
         self.start = sig[0] # 노란불 시작시간
         self.end = sig[0] + sig[1] - 1 # 노란불 종료시간
 
+
+        
 # 주기가 반복될 것이므로 1주기에 대한 정보로만 문제를 풀이한다.
-# 최대 20 ^ 5 = 3,200,000
+# 최적화 버전 lcm으로 1주기를 표현
 

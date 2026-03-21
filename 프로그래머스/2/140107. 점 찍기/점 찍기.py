23fc306c3869bbc57f1bab11845812945d9dae52
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2026-03-21 21:53:51 +0900

    [level 2] Title: 점 찍기, Time: 474.85 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.py
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.py
@@ -1,8 +1,8 @@
 import math
 def solution(k, d):
-    answer = 0
+    result = 0
     # x 기준으로 세기
     for x in range(0, d + 1, k):
-        result = math.floor(math.sqrt(d * d - x * x))
-        answer += (result // k) + 1
-    return answer 
+        data = math.floor(math.sqrt(d * d - x * x))
+        result += (data // k) + 1
+    return result 

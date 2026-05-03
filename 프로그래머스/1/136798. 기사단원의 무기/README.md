dfc8799de482fd7c12bb3d28f23e813f1ccfb82e
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2026-05-03 17:56:14 +0900

    [level 1] Title: 기사단원의 무기, Time: 1388.83 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/README.md
+++ 프로그래머스/1/136798. 기사단원의 무기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 기사단원의 무기 - 136798 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798?language=python3) 
 
 ### 성능 요약
 
-메모리: 80.2 MB, 시간: 156.82 ms
+메모리: 9.43 MB, 시간: 1388.83 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 03일 19:58:49
+2026년 05월 03일 17:56:10
 
 ### 문제 설명
 
--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,17 +1,17 @@
 def solution(number, limit, power):
-    ans = 0
+    result = 0
     
-    for i in range(1, number+1):
+    for i in range(1, number + 1):
         temp = 0
         for j in range(1, int(i ** 0.5) + 1):
             if i % j == 0:
-                if i != j*j:
+                if i != j * j:
                     temp += 2
                 else:
                     temp += 1
             if temp > limit:
                 temp = power
                 break
-        ans += temp
+        result += temp
         
-    return ans
+    return result

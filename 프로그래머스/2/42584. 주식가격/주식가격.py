b68e940997b35dae9ca952565b567147a97a28ed
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2026-06-14 23:26:12 +0900

    [level 2] Title: 주식가격, Time: 18.13 ms, Memory: 19.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,14 +1,14 @@
 def solution(prices):
     length = len(prices)
     
-    # answer을 max값으로 초기화  
-    ans = [ i for i in range (length-1, -1, -1)]
+    # result을 max값으로 초기화  
+    result = [ i for i in range (length-1, -1, -1)]
     
     # 주식 가격이 떨어질 경우 찾기
     stack = [0]
     for i in range (1, length):
         while stack and prices[stack[-1]] > prices[i]:
             j = stack.pop()
-            ans[j] = i - j
+            result[j] = i - j
         stack.append(i)
-    return ans
+    return result

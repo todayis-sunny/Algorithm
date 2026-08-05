c01625683bd03471fc33da3ca0950179679ebe7e
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2026-08-05 21:37:52 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,10 +1,10 @@
 def solution(n):
-    ans = 0
+    result = 0
     while n > 0:
         if n % 2 == 0: # 짝수이면 // 2
             n = n // 2
         else: # 홀수이면 -1
             n = n - 1
-            ans += 1 # -1 하는 횟수가 답
+            result += 1 # -1 하는 횟수가 답
     
-    return ans
+    return result

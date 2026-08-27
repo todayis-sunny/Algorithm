5344e856469bbb12f448445be1037b7d5cdda8dc
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2026-08-27 22:52:45 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 540.22 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.py
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.py
@@ -2,7 +2,7 @@ def solution(diffs, times, limit):
     maxLevel = max(diffs)  #최대 diff 구해두기
     l = 1
     r = maxLevel
-    answer = maxLevel  #정답이 max_diff인 경우를 대비해서 answer에 넣어두기
+    result = maxLevel  #정답이 max_diff인 경우를 대비해서 answer에 넣어두기
 
     while l < r:
         level = int((l + r) /2)
@@ -16,10 +16,10 @@ def solution(diffs, times, limit):
 
         if time <= limit:
             r = level
-            answer = level  
+            result = level  
             #구간이 좁아질때마다 항상 문제를 시간안에 풀 수 있는 r을 정답으로 설정
         else:
             l = level +1    
             #현재 level은 정답이 아니므로 범위 자체에서 제외
 
-    return answer
+    return result

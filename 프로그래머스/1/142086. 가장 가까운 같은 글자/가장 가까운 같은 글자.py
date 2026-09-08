a43e94a5878b8fe911ef3c4bb4353ccf12c546f5
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2026-09-08 23:58:37 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 14.77 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,14 +1,14 @@
 def solution(s):
     result = []
-    data = []
+    info = []
     for ch in s:
-        if ch in data:
-            for i in range(1, len(data) + 1):
-                if ch == data[-i]:
+        if ch in info:
+            for i in range(1, len(info) + 1):
+                if ch == info[-i]:
                     result.append(i)
                     break
-            data.append(ch)
+            info.append(ch)
         else:
             result.append(-1)
-            data.append(ch)
+            info.append(ch)
     return result

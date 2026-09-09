29518af6b155b2ff3163085bf627806d803bd739
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2026-09-09 23:47:08 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.02 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -2,12 +2,12 @@ import math
 
 def solution(n, k):
     result = []
-    number = [i for i in range(1, n + 1)]
+    info = [i for i in range(1, n + 1)]
 
-    while number:
+    while info:
         perm = math.factorial(n - 1) # 현재 자리수의 순열 개수
         idx = (k - 1) // perm # k 값을 인덱스에 맞추기 위한 1 걈소
-        result.append(number.pop(idx)) # 위에서 구한 인덱스 값을 answer에 삽입
+        result.append(info.pop(idx)) # 위에서 구한 인덱스 값을 result에 삽입
         k = k % perm # 현재 자리수의 순열 개수로 나눈 나머지로 조정
         n -= 1 # n을 줄여 다음 자리로 이동
 

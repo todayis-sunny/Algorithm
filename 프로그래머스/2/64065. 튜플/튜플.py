b9f36e7c284777f5f5e2ef0eeb26ec36dce81bbc
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2026-09-11 23:44:37 +0900

    [level 2] Title: 튜플, Time: 171.43 ms, Memory: 19.3 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,11 +1,11 @@
 def solution(s):
-    li=[]
+    li = []
     for i in s.split("},"):
         li.append(i.replace("{","").replace("}","").split(","))
-    li.sort(key=len)
-    answer=[]
+    li.sort(key = len)
+    result = []
     for i in li:
         for j in i:
-            if j not in answer:
-                answer.append(j)
-    return list(map(int,answer))
+            if j not in result:
+                result.append(j)
+    return list(map(int, result))

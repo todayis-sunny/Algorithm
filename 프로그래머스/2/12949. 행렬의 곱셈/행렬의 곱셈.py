32b27554492d25d66348c554086c5cba1d7cf0f7
Hyeongseon Jeon <77258780+todayis-sunny@users.noreply.github.com>
2026-09-15 23:26:49 +0900

    [level 2] Title: 행렬의 곱셈, Time: 31.27 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,13 +1,13 @@
 def solution(arr1, arr2):
-    ans = []
+    result = []
     m, n, r = len(arr1), len(arr1[0]), len(arr2[0])
     for i in range(m):
-        arr = arr1[i]
-        result = []
+        array = arr1[i]
+        info = []
         for j in range(r):
             hap = 0
             for k in range(n):
-                hap += arr[k] * arr2[k][j]
-            result.append(hap)
-        ans.append(result)
-    return ans
+                hap += array[k] * arr2[k][j]
+            info.append(hap)
+        result.append(info)
+    return result
